9d5753975a052dbff85c355dcb0b3f7dec2fe4a8
布田遼平 <chittai1221@gmail.com>
2019-04-21 02:44:48 +0900

    C問題

---
+++ T1PBC/C.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace T1PBC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+
+            int Rwhite = s.Count(x => x == '.');
+            int RBlack = s.Count(x => x == '#');
+
+            int LBlack = 0;
+
+            int result = Rwhite + LBlack;
+            for (int i = 0; i < input; i++)
+            {
+                if (s[i] == '.')
+                {
+                    Rwhite--;
+                }
+                else
+                {
+                    LBlack++;
+                }
+
+                if ((LBlack + Rwhite) < result)
+                {
+                    result = LBlack + Rwhite;
+                }
+
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
