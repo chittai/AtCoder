ed534fc8c68e46dd40d0c7d7c06ec6ee7bf7c9a1
布田遼平 <chittai1221@gmail.com>
2019-04-20 23:06:47 +0900

    ABC

---
+++ T1PBC/A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace T1PBC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dist = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+            if (Math.Min(dist[0], dist[1]) < dist[2] && dist[2] < Math.Max(dist[0], dist[1]))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ T1PBC/B.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace T1PBC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[] s = Console.ReadLine().ToCharArray();
+            int k = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            foreach (char c in s)
+            {
+                if (c != s[k - 1])
+                    s[count] = '*';
+                count++;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
