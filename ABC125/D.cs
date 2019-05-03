ead7e19efa619d4eacefd9cfe11bdeb92f9e6154
布田遼平 <chittai1221@gmail.com>
2019-05-03 23:31:16 +0900

    fix D.cs

--- ABC125/D.cs
+++ ABC125/D.cs
@@ -8,32 +8,30 @@ namespace ABC125
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
             string[] a = Console.ReadLine().Split(' ');
             int[] an = a.Select(x => int.Parse(x)).ToArray();
 
-            for (int i = 0; i < n - 1; i++)
-            {
-                if (an[i] < 0 && an[i + 1] < 0)
-                {
-                    an[i] *= -1;
-                    an[i + 1] *= -1;
-                }
+            long result = 0;
 
-                if (an[i] < 0 && Math.Abs(an[i]) > Math.Abs(an[i + 1]))
-                {
-                    an[i] *= -1;
-                    an[i + 1] *= -1;
-                }
-            }
-
-            int result = 0;
             foreach (int x in an)
             {
-                result += x;
+                result += Math.Abs(x);
             }
-            Console.WriteLine(result);
-        }
 
+            if (an.Count(x => x < 0) % 2 == 0)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                int min = Math.Abs(an[0]);
+                for (int i = 0; i < an.Length; i++)
+                {
+                    if (Math.Abs(an[i]) < min) min = Math.Abs(an[i]);
+                }
+                result -= min * 2;
+                Console.WriteLine(result);
+            }
+        }
     }
 }
