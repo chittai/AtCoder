7b6ce0f64d2f243d8266f623e2c5120ea70ef666
布田遼平 <chittai1221@gmail.com>
2019-05-26 23:19:02 +0900

    ABC128

---
+++ ABC128/A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC128
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int A = int.Parse(s[0]);
+            int P = int.Parse(s[1]);
+
+            int pieP = P / 2;
+            int pieP2 = P % 2;
+
+            int pieA = (A * 3 + pieP2) / 2;
+
+            int result = pieA + pieP;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ABC128/B.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC128
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int N = int.Parse(s);
+
+            string[] s1;
+
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            Dictionary<int, int> dic2 = new Dictionary<int, int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                s1 = Console.ReadLine().Split(' ');
+                dic.Add(int.Parse(s1[1]), s1[0]);
+
+                dic2.Add(int.Parse(s1[1]), i);
+            }
+
+            var SortedDic = dic.OrderBy(x => x.Value).ThenByDescending(x => x.Key);
+
+            foreach (var v in SortedDic)
+            {
+                Console.WriteLine(dic2[v.Key] + 1);
+            }
+
+        }
+    }
+}
