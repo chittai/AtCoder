3d727ae51bbb243a863ddca12abe1adf1a029170
布田遼平 <chittai1221@gmail.com>
2019-08-29 23:15:46 +0900

    Add A & B , AC

---
+++ ABC084/A.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ABC084
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int M = int.Parse(Console.ReadLine());
+
+            int res = (24 - M) + 24;
+
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ ABC084/B.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ABC084
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            string S = Console.ReadLine();
+
+            bool res;
+            for (int i = 0; i < A; i++)
+            {
+                res = Char.IsDigit(S[i]);
+                if (!res)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+
+            if (S[A] != '-')
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            for (int i = A + 1; i < A + 1 + B; i++)
+            {
+                res = Char.IsDigit(S[i]);
+                if (!res)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+            Console.WriteLine("Yes");
+        }
+    }
+}
