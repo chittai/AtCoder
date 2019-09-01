1d74f0403e4a16afb65f3e58a40fe84fcca71207
布田遼平 <chittai1221@gmail.com>
2019-09-01 23:49:13 +0900

    Add A,B & AC

---
+++ ABC137/A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC137
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            int[] n = new int[] { A + B, A - B, A * B };
+            int res = n.Max();
+
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ ABC137/B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC137
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int K = int.Parse(input[0]);
+            int X = int.Parse(input[1]);
+
+            for (int i = (K - 1) * (-1); i < K; i++)
+            {
+                Console.Write(X + i);
+                if (i < K - 1) Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
