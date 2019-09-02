6c120165393a739e0537c1b1bf113497ea081b32
布田遼平 <chittai1221@gmail.com>
2019-09-03 00:05:49 +0900

    Add A.cs ,B.cs & AC

---
+++ ABC135/A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ABC135
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            if ((A + B) % 2 == 0)
+            {
+                Console.WriteLine((A + B) / 2);
+            }
+            else
+            {
+                Console.WriteLine("IMPOSSIBLE");
+            }
+        }
+    }
+}
---
+++ ABC135/B.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC135
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+
+            int[] array = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = int.Parse(input[i]);
+            }
+
+            int conut = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (array[i] != i + 1) conut++;
+            }
+
+            if (conut == 0 || conut == 2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
