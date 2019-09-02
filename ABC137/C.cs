fea1bb23a689cda2b6c3ce0e6c5f555503dc188a
布田遼平 <chittai1221@gmail.com>
2019-09-02 10:32:46 +0900

    Add C.cs & AC

---
+++ ABC137/C.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC137
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Dictionary<string, long> dic = new Dictionary<string, long>();
+            string[] ss = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string s = Console.ReadLine();
+                ss[i] = String.Concat(s.OrderBy(a => a));
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (dic.ContainsKey(ss[i]))
+                {
+                    dic[ss[i]]++;
+                }
+                else
+                {
+                    dic.Add(ss[i], 0);
+                }
+            }
+
+            long res = 0;
+            foreach (int i in dic.Values)
+            {
+                long temp = i;
+                while (1 <= temp)
+                {
+                    res += temp;
+                    temp--;
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
