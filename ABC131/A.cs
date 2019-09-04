44c67eb227f5ba6ea7d21c6d258f56a3b2d1a9d0
布田遼平 <chittai1221@gmail.com>
2019-09-05 01:25:54 +0900

    Add A.cs, B.cs & AC

---
+++ ABC131/A.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace ABC131
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            char tmp = S[0];
+            for (int i = 1; i < 4; i++)
+            {
+                if (tmp == S[i])
+                {
+                    Console.WriteLine("Bad");
+                    return;
+                }
+                else
+                {
+                    tmp = S[i];
+                }
+            }
+            Console.WriteLine("Good");
+        }
+    }
+}
---
+++ ABC131/B.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace ABC131
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int L = int.Parse(input[1]);
+
+            int[] aji = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                aji[i] = L + i;
+            }
+
+            if (0 <= L) { aji[0] = 0; }
+            if (L < 0)
+            {
+                if (Math.Abs(L) >= Math.Abs(N))
+                {
+                    aji[N - 1] = 0;
+                }
+            }
+
+            int res = 0;
+            foreach (var k in aji)
+            {
+                res += k;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
