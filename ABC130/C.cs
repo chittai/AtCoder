8485f3f42e8e89e7e9434a593f24f62fb84ffd9d
布田遼平 <chittai1221@gmail.com>
2019-09-05 23:58:28 +0900

    Add C.cs & AC

---
+++ ABC130/C.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ABC130
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            double W = double.Parse(input[0]);
+            double H = double.Parse(input[1]);
+            double x = double.Parse(input[2]);
+            double y = double.Parse(input[3]);
+
+            if ((W % 2) == 0 && (H % 2) == 0)
+            {
+                if (x == (W / 2) && y == (H / 2))
+                {
+                    Console.Write((W * H) / 2);
+                    Console.Write(' ');
+                    Console.WriteLine(1);
+                    return;
+                }
+            }
+            Console.Write((W * H) / 2);
+            Console.Write(' ');
+            Console.WriteLine(0);
+        }
+    }
+}
