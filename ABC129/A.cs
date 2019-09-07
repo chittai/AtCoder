58e47a05d08edcd43eac16688e74442ce99ad2c1
布田遼平 <chittai1221@gmail.com>
2019-09-07 22:50:46 +0900

    Add A-B.cs & AC

---
+++ ABC129/A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC129
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int[] n = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                n[i] = int.Parse(input[i]);
+            }
+
+            Array.Sort(n);
+
+            Console.WriteLine(n[0] + n[1]);
+        }
+    }
+}
---
+++ ABC129/B.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ABC129
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+
+            int[] W = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                W[i] = int.Parse(input[i]);
+            }
+
+            int[] S = new int[N + 1];
+            S[0] = 0;
+            for (int i = 0; i < N; i++)
+            {
+                S[i + 1] += S[i] + W[i];
+            }
+
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = Math.Abs(S[N] - 2 * S[i]);
+            }
+
+            Array.Sort(A);
+
+            Console.WriteLine(A[0]);
+
+        }
+    }
+}
