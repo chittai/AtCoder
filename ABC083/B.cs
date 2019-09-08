e8fd2d936132c9b23a0fae163623a326dfc35c23
布田遼平 <chittai1221@gmail.com>
2019-09-09 00:14:21 +0900

    Add B.cs & AC

---
+++ ABC083/B.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ABC083
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int A = int.Parse(input[1]);
+            int B = int.Parse(input[2]);
+
+            int sum = 0;
+            int res = 0;
+            int value = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                value = i;
+                while (0 < value)
+                {
+                    sum += (value % 10);
+                    value = (value / 10);
+                }
+
+                if (A <= sum && sum <= B)
+                {
+                    res += i;
+                }
+                sum = 0;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
