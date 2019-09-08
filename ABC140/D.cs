34ecd613cd4063d4070664d306dff262cedd40d9
布田遼平 <chittai1221@gmail.com>
2019-09-08 17:46:28 +0900

    Add D.cs & AC

---
+++ ABC140/D.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ABC140
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string s = Console.ReadLine();
+
+            int N = int.Parse(input[0]);
+            int K = int.Parse(input[1]);
+
+            int socre = 0;
+            for (int i = 1; i < N; i++)
+            {
+                if (s[i] == s[i - 1]) socre++;
+            }
+
+            Console.WriteLine(Math.Min(socre + 2 * K, N - 1));
+        }
+    }
+}
