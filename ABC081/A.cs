bd6f982c5e8892da64635302a6ad7d02efdf569d
布田遼平 <chittai1221@gmail.com>
2019-09-08 21:18:16 +0900

    Add A-B.cs & AC

---
+++ ABC081/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC081
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s.Where(x => x == '1').Count());
+        }
+    }
+}
---
+++ ABC081/B.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC081
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = int.Parse(input[i]);
+            }
+
+            int count = 0;
+            while (true)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (A[j] % 2 != 0) { Console.WriteLine(count); return; }
+                    A[j] = A[j] / 2;
+                }
+                count++;
+            }
+        }
+    }
+}
