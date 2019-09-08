6179e086c195a1e965a3bd01a716fd7d0cb61a2f
布田遼平 <chittai1221@gmail.com>
2019-09-08 18:01:52 +0900

    Add A.cs & AC

---
+++ ABC086/A.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ABC086
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+            if ((a * b) % 2 == 0) { Console.WriteLine("Even"); }
+            else { Console.WriteLine("Odd"); }
+        }
+    }
+}
