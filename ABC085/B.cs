d5db103d6d4209e297fc7cfb5359af86c6f522b3
布田遼平 <chittai1221@gmail.com>
2019-09-09 23:31:49 +0900

    Add B.cs & AC

---
+++ ABC085/B.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC085
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] d = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                d[i] = int.Parse(Console.ReadLine());
+            }
+
+            d = d.OrderByDescending(x => x).Distinct().ToArray();
+            Console.WriteLine(d.Count());
+        }
+    }
+}
