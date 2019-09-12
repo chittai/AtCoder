3709e89d0933c757b987059f4986ff6db5bc2647
布田遼平 <chittai1221@gmail.com>
2019-09-13 00:44:50 +0900

    Add C.cs & AC

--- ABC129/C.cs
+++ ABC129/C.cs
@@ -3,41 +3,30 @@ using System.Linq;
 
 namespace ABC129
 {
-    class B
+    class C
     {
         static void Main(string[] args)
         {
             string[] input = Console.ReadLine().Split(' ');
-            long N = long.Parse(input[0]);
-            long M = long.Parse(input[1]);
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
 
             long[] a = new long[M];
             for (int i = 0; i < M; i++)
             {
                 a[i] = long.Parse(Console.ReadLine());
             }
-            Console.WriteLine(CalcStep(N - 1, a) + CalcStep(N - 1, a));
-        }
 
-        static long CalcStep(long N, long[] M)
-        {
+            long[] dp = new long[N + 10];
+            dp[N] = 1;
 
-            long ans1 = 0;
-            long ans2 = 0;
-
-            // 今の段
-            if (N == 0) { return 1; }
-            // 次の段の評価
-            if (0 <= N && !M.Contains(N))
+            int index = M - 1;
+            for (int i = N - 1; 0 <= i; i--)
             {
-                ans1 += (CalcStep(N - 1, M)) % 1000000007;
+                if (0 <= index && a[index] == i) { dp[index] = 0; index--; continue; }
+                dp[i] = (dp[i + 1] + dp[i + 2]) % 1000000007;
             }
-            if (0 <= N && !M.Contains(N))
-            {
-                ans2 += CalcStep(N - 2, M) % 1000000007;
-            }
-
-            return ans1 + ans2;
+            Console.WriteLine(dp[0]);
         }
     }
 }
