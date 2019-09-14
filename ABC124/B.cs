fb12030abb6664f2108c05a569faa8d22f1b05d1
布田遼平 <chittai1221@gmail.com>
2019-09-14 18:14:25 +0900

    Add B.cs, C.cs & AC

--- ABC124/B.cs
+++ ABC124/B.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
- 
+
 namespace ABC124.B
 {
     class B
@@ -9,24 +9,24 @@ namespace ABC124.B
         {
             string mountains = Console.ReadLine();
             string[] height = Console.ReadLine().Split(' ');
- 
+
             int intMountains = int.Parse(mountains);
             int[] intHeigt = height.Select(int.Parse).ToArray();
-  
+
             int minHeight = intHeigt[0];
             int count = 0;
- 
-            foreach(int h in intHeigt)
+
+            foreach (int h in intHeigt)
             {
-                if(h >= minHeight)
+                if (h >= minHeight)
                 {
                     minHeight = h;
                     count++;
                 }
             }
- 
+
             Console.WriteLine(count);
- 
+
         }
     }
 }
---
+++ ABC124/C.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ABC124
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            int wCount = 0;
+            int bCount = 0;
+
+            // 先頭が白のパターン
+            for (int i = 0; i < S.Length; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    if (S[i] != '1') { wCount++; }
+                }
+                if ((i % 2) == 1)
+                {
+                    if (S[i] != '0') { wCount++; }
+                }
+            }
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    if (S[i] != '0') { bCount++; }
+                }
+                if ((i % 2) == 1)
+                {
+                    if (S[i] != '1') { bCount++; }
+                }
+            }
+            Console.WriteLine(Math.Min(wCount, bCount));
+        }
+    }
+}
