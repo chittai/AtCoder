fdfef50b4185ae497491150d51c7e95da744f367
taquan <chittai1221@gmail.com>
2019-09-19 00:19:35 +0900

    Add C.cs & AC

---
+++ ABC123/C.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC123
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            double N = double.Parse(Console.ReadLine());
+
+            long[] alpha = new long[5];
+
+            for(long i = 0; i < alpha.Length; i++)
+            {
+                alpha[i] = long.Parse(Console.ReadLine());
+            }
+            
+            double min = alpha.Min();
+            double a = 4 + Math.Ceiling(N/min);
+
+            long aa = (long)a;
+
+            Console.WriteLine(aa);
+        }
+    }
+}
