a32c28178992e0c89ebebf49b57e70b31953440d
chittai <chittai1221@gmail.com>
2019-09-19 19:35:17 +0900

    Add A.cs & AC

---
+++ ABC082/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC082
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            Console.WriteLine(Math.Ceiling((s[0] + s[1]) * 0.5));
+        }
+    }
+}
