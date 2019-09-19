efd3d5f6f6799df74202eec864dd502dd761ef21
chittai <chittai1221@gmail.com>
2019-09-19 18:07:56 +0900

    Add A.cs & AC

---
+++ ABC088/A.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ABC088
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int A = int.Parse(Console.ReadLine());
+            if (0 <= A - (N % 500)) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
