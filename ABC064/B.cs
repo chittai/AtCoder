4f53d1d8c14b834f1f9e2a72171a7ada68680f2a
chittai <chittai1221@gmail.com>
2019-09-19 17:54:06 +0900

    Add B.cs & AC

---
+++ ABC064/B.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC064
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+
+            int[] a = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                a[i] = int.Parse(input[i]);
+            }
+
+            int max = a.Max();
+            int min = a.Min();
+
+            Console.WriteLine(max - min);
+        }
+    }
+}
---
+++ ABC064/B2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC064
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(a.Max() - a.Min());
+        }
+    }
+}
