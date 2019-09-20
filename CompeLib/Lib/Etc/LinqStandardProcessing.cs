e9d12bfa7e5b90f8ae7a0aff37ff9560ca87c973
taquan <chittai1221@gmail.com>
2019-09-20 19:20:06 +0900

    Add LinqStandardProcessing Library

---
+++ CompeLib/Lib/Etc/LinqStandardProcessing.cs
@@ -0,0 +1,24 @@
+/* AtCoderで使用する定番のLinqの処理をまとめる */
+
+using System;
+using System.Linq;
+
+namespace Etc
+{
+    class LinqStandardProcessing
+    {
+        static void Main(string[] args)
+        {
+
+            int N;
+            string S;
+
+            // 数列を入力して、文字列から整数へと変換する
+            N = Console.ReadLine().Select(int.Parse).ToArray();
+
+            // Linqで処理した結果文字列へ変換する
+            // 下記は、Linqを使用したインデックス処理も併せて記載している
+            S = new string(Console.ReadLine().Where((x, index) => index % 2 == 0).ToArray());
+        }
+    }
+}
