7397dee329d0e3a0abbb13ff770d14e00b1765a0
taquan <chittai1221@gmail.com>
2019-09-20 19:06:20 +0900

    Add B.cs & AC

---
+++ ABC053/B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC053
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int count = 0;
+            int a = 0;
+            bool isFirst = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'A' && isFirst) { a = i; isFirst = false; }
+                if (s[i] == 'Z') { count = (i - a) + 1; }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ ABC053/B2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ABC053
+{
+    class B2
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s.LastIndexOf('Z') - s.IndexOf('A') + 1);
+        }
+    }
+}
