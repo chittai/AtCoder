fdad45bdff154b59f45f00e7bd936295933bd174
taquan <chittai1221@gmail.com>
2019-09-20 19:41:35 +0900

    Add B.cs & AC

--- ABC095/B.cs
+++ ABC095/B.cs
@@ -7,8 +7,22 @@ namespace ABC095
     {
         static void Main(string[] args)
         {
-            int count = Console.ReadLine().Count(x => x == 'o');
-            Console.WriteLine(700 + count * 100);
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int X = int.Parse(input[1]);
+
+            int min = int.MaxValue;
+            int m = 0;
+            int count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                m = int.Parse(Console.ReadLine());
+                X -= m;
+                count++;
+                min = Math.Min(min, m);
+            }
+
+            Console.WriteLine(count + X / min);
         }
     }
 }
