40c1bb6548e55ad44c96775714e0ed8e9aea9ae8
taquan <chittai1221@gmail.com>
2019-09-21 01:38:25 +0900

    Add A.cs & AC

---
+++ AGC025/A.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AGC025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int min = int.MaxValue;
+            int sum = 0;
+            int nA = 0;
+            int nB = 0;
+
+            for (int i = 1; i < N; i++)
+            {
+                nA = i;
+                nB = N - i;
+                while (0 < nA)
+                {
+                    sum += nA % 10;
+                    nA = nA / 10;
+                }
+
+                while (0 < nB)
+                {
+                    sum += nB % 10;
+                    nB = nB / 10;
+                }
+
+                min = Math.Min(min, sum);
+                sum = 0;
+
+            }
+
+            Console.WriteLine(min);
+        }
+    }
+}
