8b59763f864b1d844e51ae09403f8ec8dd47372a
taquan <chittai1221@gmail.com>
2019-09-21 01:05:16 +0900

    Add B.cs & AC

---
+++ ABC090/B.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ABC090
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            int count = 0;
+            string s;
+
+            for (int i = A; i <= B; i++)
+            {
+                s = i.ToString();
+
+                if (s[0] == s[s.Length - 1] && s[1] == s[s.Length - 2]) { count++; }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
