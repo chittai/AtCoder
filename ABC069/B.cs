f64d1bddf861371c187f0d145ae226b9f02c420e
taquan <chittai1221@gmail.com>
2019-09-20 15:02:42 +0900

    Add B.cs & AC

---
+++ ABC069/B.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ABC069
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.Write(s[0]);
+            Console.Write(s.Length - 2);
+            Console.WriteLine(s[s.Length - 1]);
+        }
+    }
+}
