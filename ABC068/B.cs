ca1acdd607c53d5016a0aa81bd6ab440c2f13d64
taquan <chittai1221@gmail.com>
2019-09-20 16:28:14 +0900

    Add B.cs & AC

---
+++ ABC068/B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC068
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            double res = 0;
+            int i = 0;
+            while (res <= N)
+            {
+                res = Math.Pow(2, i);
+                i++;
+            }
+            Console.WriteLine(res / 2);
+        }
+    }
+}
