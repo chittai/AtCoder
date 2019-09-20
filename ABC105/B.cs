088e085fed3a355b9848edf33432c455a3d7e489
taquan <chittai1221@gmail.com>
2019-09-20 22:54:24 +0900

    Add B.cs & AC

---
+++ ABC105/B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC105
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            if (N % 4 == 0 || N % 7 == 0) { Console.WriteLine("Yes"); return; }
+
+            while (6 < N)
+            {
+                N -= 4;
+                if (N % 7 == 0) { Console.WriteLine("Yes"); return; }
+            }
+            Console.WriteLine("No");
+        }
+    }
+}
