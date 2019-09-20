9edc0b88c7765c380b2124135e4f77e23450f178
taquan <chittai1221@gmail.com>
2019-09-21 01:38:46 +0900

    Modify LSP

--- CompeLib/Lib/Etc/LinqStandardProcessing.cs
+++ CompeLib/Lib/Etc/LinqStandardProcessing.cs
@@ -10,11 +10,27 @@ namespace Etc
         static void Main(string[] args)
         {
 
+            // 数の処理に使用 
             int N;
+            int[] NA;
+            int a;
+            int b;
+            // 文字列の処理に使用
             string S;
+
+            /* 以下、処理のまとめ */
 
             // 数列を入力して、文字列から整数へと変換する
             N = Console.ReadLine().Select(int.Parse).ToArray();
+
+            // 違う用途の数列を入力して各対応する変数に格納する
+            NA = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            a = NA[0];
+            b = NA[1];
+
+            // 配列の要素数をカウントする
+            // Count には条件を指定できる
+            N = Console.ReadLine().Count(x => x == 'o');
 
             // Linqで処理した結果文字列へ変換する
             // 下記は、Linqを使用したインデックス処理も併せて記載している
