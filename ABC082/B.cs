98530c690057ca2e68c20e34cbb43bc5e2733626
taquan <chittai1221@gmail.com>
2019-09-20 15:02:50 +0900

    Add B.cs & AC

---
+++ ABC082/B.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+namespace ABC082
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string s = new string(Console.ReadLine().OrderBy(x => x).ToArray());
+            string t = new string(Console.ReadLine().OrderByDescending(x => x).ToArray());
+
+            Console.WriteLine(s.CompareTo(t) == -1 ? "Yes" : "No");
+        }
+    }
+}
