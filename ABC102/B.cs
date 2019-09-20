5eeb183456ee5ef4f4603363b68f5e08fb7e6d6c
taquan <chittai1221@gmail.com>
2019-09-20 16:35:15 +0900

    Add B.cs & AC

---
+++ ABC102/B.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC102
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            var A = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(A.Max() - A.Min());
+        }
+    }
+}
