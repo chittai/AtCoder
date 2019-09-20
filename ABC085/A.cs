d53e966b32f82ef2d4db24253d6dc1c0ef8bc2f9
taquan <chittai1221@gmail.com>
2019-09-20 14:23:14 +0900

    Add A-B.cs & AC

---
+++ ABC085/A.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC085
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine().Replace("2017", "2018"));
+        }
+    }
+}
