43bdb491b508832ff693dc7aec362075b4bf471d
taquan <chittai1221@gmail.com>
2019-09-20 18:50:23 +0900

    Add B.cs & AC

---
+++ ABC072/B.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC072
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new string(Console.ReadLine().Where((x, index) => index % 2 == 0).ToArray()));
+        }
+    }
+}
