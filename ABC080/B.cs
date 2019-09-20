d50b8ed2222e37148c309cee134c05b95f12e5c4
taquan <chittai1221@gmail.com>
2019-09-21 00:53:36 +0900

    Add B.cs & AC

---
+++ ABC080/B.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC080
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int n = N;
+            int sum = 0;
+            while (0 < n)
+            {
+                sum += n % 10;
+                n = n / 10;
+            }
+
+            if (N % sum == 0) { Console.WriteLine("Yes"); } else { Console.WriteLine("No"); }
+
+        }
+    }
+}
