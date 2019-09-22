85b876ab8ee4e39b5dcd15d09c8d72acf4179c2e
布田遼平 <chittai1221@gmail.com>
2019-09-22 23:59:50 +0900

    Add B.cs & AC

---
+++ ABC042/B.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC042
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int N = int.Parse(input[0]);
+            int L = int.Parse(input[1]);
+
+            string[] s = new string[N];
+            for (int i = 0; i < N; i++)
+            {
+                s[i] = Console.ReadLine();
+            }
+
+            s = s.OrderBy(x => x).ToArray();
+
+            string res = "";
+
+            for (int i = 0; i < N; i++)
+            {
+                res += s[i];
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
