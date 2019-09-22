f796f7d01489ae37b6452470886952c7379ec8a0
布田遼平 <chittai1221@gmail.com>
2019-09-23 00:14:54 +0900

    Add A.cs & AC

---
+++ ABC027/A.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC027
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int x = int.Parse(input[1]);
+
+            int[] a = Console.ReadLine().Split().Select(int.Parse).OrderBy(y => y).ToArray();
+
+            int count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                x -= a[i];
+                if (0 <= x) count++;
+                if (x < 0) break;
+            }
+            if (0 < x) count--;
+            Console.WriteLine(count);
+        }
+    }
+}
