a455f3e071034d806b197c27d49efe7d23d08d40
布田遼平 <chittai1221@gmail.com>
2019-09-22 23:38:33 +0900

    Add B.cs & AC

---
+++ ABC067/B.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC067
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int K = int.Parse(input[1]);
+
+            int[] l = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToArray();
+
+            int res = 0;
+
+            for (int i = 0; i < K; i++)
+            {
+                res += l[i];
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
