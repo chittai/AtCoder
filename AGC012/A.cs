1c84081ee62d5b576bd79d5e5bf9ad852b3feb98
布田遼平 <chittai1221@gmail.com>
2019-09-23 00:59:33 +0900

    Add A.cs & AC

---
+++ AGC012/A.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC012
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            long N = int.Parse(Console.ReadLine());
+            long[] a = Console.ReadLine().Split().Select(long.Parse).OrderBy(x => x).ToArray();
+
+            long i = 0;
+            long l = a.Length - 2;
+
+            long sum = 0;
+            while (i < l)
+            {
+                sum += a[l];
+                l = l - 2;
+                i++;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
