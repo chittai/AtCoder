758b3d5ac93d8fe4ff8cbe65211fca0f7c71acbd
布田遼平 <chittai1221@gmail.com>
2019-09-24 00:04:40 +0900

    Add B.cs & AC

---
+++ ABC091/B.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC091
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            string s = "";
+            for (int i = 0; i < N; i++)
+            {
+                s = Console.ReadLine();
+                if (!dict.ContainsKey(s)) { dict.Add(s, 0); }
+                if (dict.ContainsKey(s)) { dict[s]++; }
+            }
+
+            int M = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < M; i++)
+            {
+                s = Console.ReadLine();
+                if (!dict.ContainsKey(s)) { dict.Add(s, 0); }
+                if (dict.ContainsKey(s)) { dict[s]--; }
+            }
+
+            var res = dict.OrderByDescending(x => x.Value);
+
+            if (res.First().Value < 0) Console.WriteLine(0);
+            else Console.WriteLine(res.First().Value);
+        }
+    }
+}
