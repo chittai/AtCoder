a3e8019e8befd88a623df49c2f0a8ca3675aacb6
布田遼平 <chittai1221@gmail.com>
2019-09-23 16:15:15 +0900

    Add B.cs & AC

---
+++ ABC071/B.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ABC071
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            var S = Console.ReadLine();
+
+            for (var i = 'a'; i <= 'z'; i++)
+            {
+                if (!S.Contains(i.ToString())) { Console.WriteLine(i); return; }
+            }
+            Console.WriteLine("None");
+        }
+    }
+}
