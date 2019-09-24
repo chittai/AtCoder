ce6376dc4fb4900bd2f8d0f909ec24f2a4d3e6d3
布田遼平 <chittai1221@gmail.com>
2019-09-24 15:30:20 +0900

    Add C.cs & AC

---
+++ ABC057/C.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ABC057
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+
+            long min = long.MaxValue;
+            long B = 0;
+            for (long i = 1; i <= Math.Sqrt(N); i++)
+            {
+                if (N % i == 0)
+                {
+                    B = N / i;
+
+                    min = Math.Min(min, CalculateTheNumberOfRows(B));
+                }
+            }
+
+            Console.WriteLine(min);
+        }
+
+        static long CalculateTheNumberOfRows(long x)
+        {
+            long count = 0;
+            while (0 < x)
+            {
+                x /= 10;
+                count++;
+
+            }
+            return count;
+        }
+    }
+}
