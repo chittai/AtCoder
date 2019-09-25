c6aaeb442ef700d39ee148c6f73c270974899fdb
布田遼平 <chittai1221@gmail.com>
2019-09-25 11:39:50 +0900

    Add C.cs & AC

---
+++ ABC059/C.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC059
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+
+            long n = int.Parse(Console.ReadLine());
+            long[] a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            Console.WriteLine(Math.Min(CalcA(a, true), CalcA(a, false)));
+        }
+        static long CalcA(long[] a, bool isP)
+        {
+            long result = 0;
+            long sum = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+                if (isP)
+                {
+                    if (sum <= 0) { result += 1 - sum; sum = 1; }
+                }
+                else
+                {
+                    if (0 <= sum) { result += 1 + sum; sum = -1; }
+                }
+
+                isP = isP ? false : true;
+
+            }
+            return result;
+        }
+    }
+}
+
