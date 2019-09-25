29636ce9dea1ec0c0db6f2d38ae6feea7f17cc98
布田遼平 <chittai1221@gmail.com>
2019-09-25 23:22:14 +0900

    Add C.cs & AC

---
+++ ABC073/C.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC073
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            //int[] A = new int[N];
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int a = 0;
+            for (int i = 0; i < N; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+                if (!dict.ContainsKey(a)) { dict.Add(a, 0); }
+                if (dict.ContainsKey(a)) { dict[a]++; }
+            }
+            Console.WriteLine(dict.Count(x => (x.Value) % 2 != 0));
+        }
+    }
+}
