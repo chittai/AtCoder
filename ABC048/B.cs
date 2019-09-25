04f2c86ad6395919ef539ac662f2c316cd26c75e
布田遼平 <chittai1221@gmail.com>
2019-09-26 01:26:17 +0900

    Add B.cs & AC

---
+++ ABC048/B.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ABC048
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            long a = long.Parse(input[0]);
+            long b = long.Parse(input[1]);
+            long x = long.Parse(input[2]);
+
+            long start = 0;
+            if (a % x == 0) start = (a / x) - 1;
+            else start = a / x;
+
+            long end = b / x;
+            Console.WriteLine(end - start);
+        }
+    }
+}
