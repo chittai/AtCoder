3890f3dcef5067b480692604adffe16a0bab282e
布田遼平 <chittai1221@gmail.com>
2019-09-25 22:53:45 +0900

    Add A.cs & AC

---
+++ AGC010/A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC010
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (n[i] % 2 != 0) count++;
+            }
+
+            if (count % 2 != 0) Console.WriteLine("NO");
+            else Console.WriteLine("YES");
+        }
+    }
+}
