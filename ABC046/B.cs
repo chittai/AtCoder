1e9e2f1a3d72887ebcb551070c0a20031243d1ca
布田遼平 <chittai1221@gmail.com>
2019-09-26 01:06:36 +0900

    Add B.cs & AC

---
+++ ABC046/B.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ABC046
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int K = int.Parse(input[1]);
+
+            double res = K * Math.Pow(K - 1, N - 1);
+            Console.WriteLine(res);
+        }
+    }
+}
