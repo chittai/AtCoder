0aba5f99a89d64882782eae88545d7e0e4d7c743
布田遼平 <chittai1221@gmail.com>
2019-09-26 00:56:26 +0900

    Add B.cs & AC

---
+++ ABC070/B.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ABC070
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] intput = Console.ReadLine().Split();
+            int A = int.Parse(intput[0]);
+            int B = int.Parse(intput[1]);
+            int C = int.Parse(intput[2]);
+            int D = int.Parse(intput[3]);
+
+            Console.WriteLine(Math.Max(0, Math.Min(B, D) - Math.Max(A, C)));
+        }
+    }
+}
