e745960530e55ca748903783f1b67e339feed671
布田遼平 <chittai1221@gmail.com>
2019-09-25 22:42:22 +0900

    Add C.cs & AC

---
+++ ABC093/C.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC093
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int[] ABC = new int[3] { int.Parse(input[0]), int.Parse(input[1]), int.Parse(input[2]) };
+
+            int max = ABC.Max();
+            if ((3 * max) % 2 != (ABC[0] + ABC[1] + ABC[2]) % 2) { max = ABC.Max() + 1; }
+
+            Console.WriteLine(((3 * max) - (ABC[0] + ABC[1] + ABC[2])) / 2);
+        }
+    }
+}
