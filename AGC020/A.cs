956eb4e3067aeedea5b6259eee496aec98d4e551
布田遼平 <chittai1221@gmail.com>
2019-09-25 23:07:24 +0900

    Add A.cs & AC

---
+++ AGC020/A.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AGC020
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int A = int.Parse(input[1]);
+            int B = int.Parse(input[2]);
+            if ((B - A - 1) % 2 != 0) Console.WriteLine("Alice");
+            if ((B - A - 1) % 2 == 0) Console.WriteLine("Borys");
+        }
+    }
+}
