50db60c3be7451b2e991babb1af02241f2edaa07
布田遼平 <chittai1221@gmail.com>
2019-09-25 18:32:12 +0900

    Add C.cs & AC

---
+++ ABC072/C.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC072
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] res = new int[a.Max() + 2];
+
+            foreach (var item in a)
+            {
+                res[item + 1] += 1;
+                res[item] += 1;
+                if (0 < item) res[item - 1] += 1;
+
+            }
+            Console.WriteLine(res.Max());
+        }
+    }
+}
