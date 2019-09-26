17f6cac2e4f5b11f3c8d6c798fca6b4e84098622
布田遼平 <chittai1221@gmail.com>
2019-09-26 16:28:35 +0900

    Add B.cs & AC

---
+++ ABC065/B.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC065
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] A = new int[N + 1];
+
+            for (int i = 1; i <= N; i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
+            int count = 1;
+            int a = 1;
+            for (int i = 0; i < N; i++)
+            {
+                if (A[a] == 2) { Console.WriteLine(count); return; }
+                a = A[a];
+                count++;
+
+            }
+            Console.WriteLine(-1);
+        }
+    }
+}
