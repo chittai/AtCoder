80fe5d8d2e3202f3008e38691955facbf061da6e
布田遼平 <chittai1221@gmail.com>
2019-09-27 08:42:34 +0900

    Add C.cs & AC

---
+++ ABC079/C.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ABC079
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int A = (char)input[0] - '0';
+            int B = (char)input[1] - '0';
+            int C = (char)input[2] - '0';
+            int D = (char)input[3] - '0';
+
+            if (A + B + C + D == 7) { Console.WriteLine(A.ToString() + "+" + B.ToString() + "+" + C.ToString() + "+" + D.ToString() + "=7"); }
+            else if (A + B + C - D == 7) { Console.WriteLine(A.ToString() + "+" + B.ToString() + "+" + C.ToString() + "-" + D.ToString() + "=7"); }
+            else if (A + B - C - D == 7) { Console.WriteLine(A.ToString() + "+" + B.ToString() + "-" + C.ToString() + "-" + D.ToString() + "=7"); }
+            else if (A + B - C + D == 7) { Console.WriteLine(A.ToString() + "+" + B.ToString() + "-" + C.ToString() + "+" + D.ToString() + "=7"); }
+            else if (A - B + C + D == 7) { Console.WriteLine(A.ToString() + "-" + B.ToString() + "+" + C.ToString() + "+" + D.ToString() + "=7"); }
+            else if (A - B + C - D == 7) { Console.WriteLine(A.ToString() + "-" + B.ToString() + "+" + C.ToString() + "-" + D.ToString() + "=7"); }
+            else if (A - B - C - D == 7) { Console.WriteLine(A.ToString() + "-" + B.ToString() + "-" + C.ToString() + "-" + D.ToString() + "=7"); }
+            else if (A - B - C + D == 7) { Console.WriteLine(A.ToString() + "-" + B.ToString() + "-" + C.ToString() + "+" + D.ToString() + "=7"); }
+        }
+    }
+}
