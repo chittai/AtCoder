41a7189eed76d3829267c19070ada420bd51909b
布田遼平 <chittai1221@gmail.com>
2019-09-26 15:46:06 +0900

    Add B.cs & AC

---
+++ ABC060/B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ABC060
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            int C = int.Parse(input[2]);
+
+            for (int i = 1; i <= A * B; i++)
+            {
+                if (i % A == 0 && i % B == C) { Console.WriteLine("YES"); return; }
+
+            }
+            Console.WriteLine("NO");
+        }
+    }
+}
