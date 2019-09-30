3e6aef4490b69fefebe02d2239efd17eac5c2616
布田遼平 <chittai1221@gmail.com>
2019-10-01 02:15:15 +0900

    Modify D.cs , Change int to long

--- ABC139/D.cs
+++ ABC139/D.cs
@@ -7,7 +7,7 @@ namespace ABC139
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
+            long N = int.Parse(Console.ReadLine());
             long res = 0;
             for (long i = 0; i < N; i++)
             {
