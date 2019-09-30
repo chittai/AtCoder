f8a641d0bef9de3ef7d38fafbc922591dedb5210
布田遼平 <chittai1221@gmail.com>
2019-09-30 23:55:47 +0900

    Add A-B.cs & AC

---
+++ ABC120/A.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ABC120
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            int C = int.Parse(input[2]);
+
+            if (B / A < C) Console.WriteLine(B / A);
+            else Console.WriteLine(C);
+        }
+    }
+}
---
+++ ABC120/B.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABC120
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            int K = int.Parse(input[2]);
+
+            int n = CalcGCD(A, B);
+            List<int> l = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (A % i == 0 && B % i == 0) { l.Add(i); }
+            }
+
+            Console.WriteLine(l[l.Count - K]);
+        }
+
+        static int CalcGCD(int A, int B)
+        {
+            if (B == 0) { return A; }
+
+            return CalcGCD(B, A % B);
+        }
+
+    }
+}
