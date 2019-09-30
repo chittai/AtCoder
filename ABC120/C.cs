f3476803566e7c3c1a960ffa7a258bcda00dce89
布田遼平 <chittai1221@gmail.com>
2019-10-01 00:11:45 +0900

    Add C.cs & AC

---
+++ ABC120/C.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC120
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            Console.WriteLine(2 * Math.Min(S.Count(x => x == '0'), S.Count(x => x == '1')));
+        }
+    }
+}
