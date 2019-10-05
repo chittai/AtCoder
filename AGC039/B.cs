20adccdaaa73ef7532c72b078f0307d27fc8d5e7
布田遼平 <chittai1221@gmail.com>
2019-10-05 23:33:23 +0900

    Add B.cs & TimeUp

---
+++ AGC039/B.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AGC039
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+
+            int[,] S = new int[N, N];
+            string s;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    s = Console.ReadLine();
+                    S[i, j] = int.Parse(s[j].ToString());
+                }
+            }
+
+            List<int> Dup = new List<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (S[i, j] == 1) Dup.Add(j);
+                }
+
+                foreach (var item in Dup)
+                {
+                    if (S[i, item] == 1) { Console.WriteLine("No"); return; }
+                }
+            }
+        }
+    }
+}
