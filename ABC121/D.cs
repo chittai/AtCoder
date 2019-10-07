5a776ecdd2a4a6bea3556cf577ad1c727e94f58b
布田遼平 <chittai1221@gmail.com>
2019-10-07 23:28:36 +0900

    Add D.cs & AC

---
+++ ABC121/D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC121
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long A = input[0]; long B = input[1];
+
+            Console.WriteLine(F(B) ^ F(A - 1));
+        }
+
+        static long F(long x)
+        {
+            switch (x % 4)
+            {
+                case 0: return x;
+                case 1: return 1;
+                case 2: return x + 1;
+                case 3: return 0;
+                default: return 0;
+            }
+        }
+
+    }
+}
