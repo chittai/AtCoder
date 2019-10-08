3655715849016493af8ff6ba90909df4b5c318aa
布田遼平 <chittai1221@gmail.com>
2019-10-09 01:42:55 +0900

    Add Mathmatical.cs

---
+++ CompeLib/Lib/Etc/Mathematical.cs
@@ -0,0 +1,34 @@
+/* 数学的処理をまとめる */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Etc
+{
+    class Combination
+    {
+        // Combination
+        static int[,] c;
+        public static int[,] init(int x)
+        {
+            c = new int[x + 5, x + 5];
+            c[0, 0] = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0) c[i, j] = c[i - 1, j];
+                    else c[i, j] = c[i - 1, j] + c[i - 1, j - 1];
+                }
+            }
+            return c;
+        }
+
+        public static int CalcCombination(int n, int k)
+        {
+            return c[n, k];
+        }
+
+    }
+}
