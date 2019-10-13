68086e068a5d5ca7448d958c0b35aa22094bf352
布田遼平 <chittai1221@gmail.com>
2019-10-14 00:53:27 +0900

    Add C.cs & AC

---
+++ D2019PC/C.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D2019PC
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+
+            long N = long.Parse(Console.ReadLine());
+
+            List<long> list = new List<long>();
+            for (long i = 1; i < Math.Sqrt(N); i++)
+            {
+                if (N % i == 0)
+                {
+                    list.Add((N / i) - 1);
+                }
+            }
+
+            long count = 0;
+            foreach (var i in list)
+            {
+                if (N % i != 0) count += i;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
