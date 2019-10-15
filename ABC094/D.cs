4baf6423314e9231c839ac153874bf7272ce804e
布田遼平 <chittai1221@gmail.com>
2019-10-16 00:45:27 +0900

    Add D.cs & AC

---
+++ ABC094/D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC094
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> a = Console.ReadLine().Split().Select(int.Parse).ToList();
+            a = a.OrderBy(x => x).ToList();
+
+            int max = a[a.Count - 1];
+            int index = a.BinarySearch(max / 2);
+            int minIndex2 = 0;
+
+            if (index < 0)
+            {
+                index = ~index;
+                minIndex2 = index - 1;
+                index = Math.Abs(a[index] - max / 2d) < Math.Abs(a[minIndex2] - max / 2d) ? index : minIndex2;
+            }
+
+            int min = a[index];
+            Console.WriteLine("{0} {1}", max, min);
+
+        }
+    }
+}
