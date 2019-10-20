cf03730e2459790c07ff1f3197abe4e125f3f816
布田遼平 <chittai1221@gmail.com>
2019-10-20 23:51:18 +0900

    Add D.cs & AC

---
+++ ABC143/D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC143
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var L = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToArray();
+
+            var indexMax = 0;
+            var count = 0;
+
+            for (int i = 0; i < N - 2; i++)
+            {
+                for (int j = i + 1; j < N - 1; j++)
+                {
+                    //indexMax = Array.BinarySearch(L, L[i] + L[j]);
+                    //if (indexMax < 0) indexMax = ~indexMax;
+                    //count += indexMax - (j + 1);
+
+                    for (int k = N - 1; j < k; k--)
+                    {
+                        if (L[k] < L[i] + L[j])
+                        {
+                            count += k - j;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
