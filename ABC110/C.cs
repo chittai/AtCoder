911f77597ac28877f4d04c964912003db403db7c
布田遼平 <chittai1221@gmail.com>
2019-10-23 23:41:47 +0900

    Add C.cs & AC

---
+++ ABC110/C.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC110
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            string T = Console.ReadLine();
+
+            int[] start = new int[26].Select(x => -1).ToArray();
+            int[] goal = new int[26].Select(x => -1).ToArray();
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                int a = S[i] - 'a';
+                int b = T[i] - 'a';
+
+                if (start[a] != -1 || goal[b] != -1)
+                {
+                    if (start[a] != b || goal[b] != a)
+                    {
+                        Console.WriteLine("No");
+                        return;
+                    }
+                }
+                start[a] = b;
+                goal[b] = a;
+            }
+            Console.WriteLine("Yes");
+        }
+    }
+}
