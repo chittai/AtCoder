5e0007971656c4d86b58fa990fe23de75537c64d
布田遼平 <chittai1221@gmail.com>
2019-10-25 00:01:42 +0900

    Add B.cs & AC

---
+++ CF2016qC/B.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace CF2016qC
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int K = input[0];
+            int T = input[1];
+
+            int[] a = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToArray();
+
+            int max = a[0];
+
+            for (int i = 1; i < T; i++)
+            {
+                max -= a[i];
+                if (max <= 0) { Console.WriteLine(0); return; }
+            }
+
+            Console.WriteLine(max - 1);
+        }
+    }
+}
