2724c251d96bcd09913d03049b117d5189ece5cd
布田遼平 <chittai1221@gmail.com>
2019-10-25 21:33:54 +0900

    Add C.cs & AC

---
+++ ABC116/C.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC116
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] h = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            long count = 0;
+            for (int i = 0; i < N - 1; i++)
+            {
+                if (h[i + 1] < h[i]) count += h[i] - h[i + 1];
+            }
+
+            count += h[N - 1];
+
+            Console.WriteLine(count);
+        }
+    }
+}
