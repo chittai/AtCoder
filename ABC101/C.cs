c25aec7fa41e42383fde2b6a2a76fe14eea45eea
布田遼平 <chittai1221@gmail.com>
2019-10-26 00:23:51 +0900

    Add C.cs & AC

---
+++ ABC101/C.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC101
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = input[0];
+            int K = input[1];
+
+            int[] A = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = 0;
+            int res = 1;
+            while (true)
+            {
+                if (N <= (K + n * K - n)) break;
+                n++;
+                res++;
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
