fa28cafeec710724b79d7e10cb1b85c03d166672
布田遼平 <chittai1221@gmail.com>
2019-10-26 21:51:52 +0900

    Add A.cs & AC

---
+++ AGC015/A.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC015
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long N = input[0];
+            long A = input[1];
+            long B = input[2];
+
+            if (N == 1)
+            {
+                if (A == B) { Console.WriteLine(1); return; }
+                else { Console.WriteLine(0); return; }
+            }
+            else
+            {
+                if (B < A) { Console.WriteLine(0); return; }
+                else Console.WriteLine((B - A) * (N - 2) + 1);
+            }
+        }
+    }
+}
