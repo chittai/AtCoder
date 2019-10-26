0fa12cfbb11502a461bd712767d949b42c41c452
布田遼平 <chittai1221@gmail.com>
2019-10-26 22:03:29 +0900

    Add A.cs & AC

---
+++ ABC006/A.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ABC006
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            if (N % 3 == 0) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+
+        }
+    }
+}
