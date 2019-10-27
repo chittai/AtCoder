8145731bfd41210e8b1fb97bd21c8e2bcc1f3486
布田遼平 <chittai1221@gmail.com>
2019-10-28 00:49:09 +0900

    Add A.cs & AC

---
+++ CF2016qB/A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CF2016qB
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string k = "CODEFESTIVAL2016";
+            int count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != k[i]) count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
