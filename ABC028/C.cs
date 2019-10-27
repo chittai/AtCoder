d22464f8d6a3be63339d8d3b4ceec23bb1acf789
布田遼平 <chittai1221@gmail.com>
2019-10-27 23:10:11 +0900

    Add C.cs & AC

---
+++ ABC028/C.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC028
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int[] alpha = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> l = new List<int>();
+            for (int i = 0; i < alpha.Length - 2; i++)
+            {
+                for (int j = i + 1; j < alpha.Length - 1; j++)
+                {
+                    for (int k = j + 1; k < alpha.Length; k++)
+                    {
+                        l.Add(alpha[i] + alpha[j] + alpha[k]);
+                    }
+                }
+            }
+
+            l = l.OrderByDescending(x => x).ToList();
+            Console.WriteLine(l[2]);
+
+        }
+    }
+}
---
+++ ABC028/C2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC028
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int[] alpha = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Math.Max(alpha[4] + alpha[3] + alpha[0], alpha[4] + alpha[2] + alpha[1]));
+        }
+    }
+}
