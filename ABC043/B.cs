81d47f482173988807f5ec218bae9bbe6c6db66b
布田遼平 <chittai1221@gmail.com>
2019-10-27 23:50:42 +0900

    Add B.cs & AC

---
+++ ABC043/B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABC043
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            List<char> l = new List<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '0') l.Add('0');
+                if (s[i] == '1') l.Add('1');
+                if (s[i] == 'B' && 0 < l.Count) l.RemoveAt(l.Count - 1);
+            }
+            Console.WriteLine(string.Join("", l));
+        }
+    }
+}
