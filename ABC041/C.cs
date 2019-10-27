1a058025f7cceefe780c2ace4d558cc267573378
布田遼平 <chittai1221@gmail.com>
2019-10-28 01:01:29 +0900

    Add C.cs & AC

---
+++ ABC041/C.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC041
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            for (int i = 0; i < N; i++)
+            {
+                d.Add(i, a[i]);
+            }
+            var d2 = d.OrderByDescending(x => x.Value);
+            foreach (var i in d2)
+            {
+                Console.WriteLine(i.Key + 1);
+            }
+        }
+    }
+}
