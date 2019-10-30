602c46c7e74a8dfbb5c53b8513ad8dec1943ba5f
布田遼平 <chittai1221@gmail.com>
2019-10-31 06:45:55 +0900

    Add B.cs & AC

---
+++ ABC022/B.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ABC022
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int X = 100000;
+            int N = int.Parse(Console.ReadLine());
+            int[] Res = new int[100000 + 1];
+
+            for (int i = 1; i < N + 1; i++)
+            {
+                int A = int.Parse(Console.ReadLine());
+                Res[A]++;
+            }
+
+            int res = 0;
+            for (int i = 1; i < X + 1; i++)
+            {
+                if (2 <= Res[i]) { res += Res[i] - 1; }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
