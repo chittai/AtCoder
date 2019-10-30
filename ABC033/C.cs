b8cecbf4ec8ce46a4b3d9de0b4eb1f4834a8892b
布田遼平 <chittai1221@gmail.com>
2019-10-31 07:13:13 +0900

    Add C.cs & AC

---
+++ ABC033/C.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC033
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            string[] S = Console.ReadLine().Split('+');
+            int count = 0;
+            foreach (var s in S)
+            {
+                if (!s.Contains('0')) { count++; }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
