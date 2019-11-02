dbf8e048611e056acc9a6bc5dd5dc1a90900f8e0
布田遼平 <chittai1221@gmail.com>
2019-11-02 22:30:28 +0900

    Add B.cs & AC

---
+++ AGC003/B.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AGC003
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+
+            long[] A = new long[N];
+            for (long i = 0; i < N; i++)
+            {
+                A[i] = long.Parse(Console.ReadLine());
+            }
+
+            long sum = 0;
+            long count = 0;
+            for (long i = 0; i < N; i++)
+            {
+                while (i < N && A[i] != 0)
+                {
+                    count += A[i];
+                    i++;
+                }
+                sum += count / 2;
+                count = 0;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
