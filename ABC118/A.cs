5c99eaf7f94ba2bb3fafc8f926c69889ca80223a
布田遼平 <chittai1221@gmail.com>
2019-11-03 19:29:56 +0900

    Add A.cs & AC

---
+++ ABC118/A.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ABC118
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int A = input[0];
+            int B = input[1];
+            if (B % A == 0) Console.WriteLine(A + B);
+            else Console.WriteLine(B - A);
+        }
+    }
+}
