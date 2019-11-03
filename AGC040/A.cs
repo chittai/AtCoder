1f6739003a3e67df1b76297a5ebea875487a2e4d
布田遼平 <chittai1221@gmail.com>
2019-11-04 08:41:35 +0900

    Add A.cs & AC

---
+++ AGC040/A.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AGC040
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"./testcase/in/A/testcase1");
+            string S = sr.ReadLine();
+            //string S = Console.ReadLine();
+            long[] res = new long[S.Length + 1];
+            long index = 1;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == '<') { res[i + 1] = index; index++; }
+                else { index = 1; }
+            }
+
+            index = 1;
+            for (int i = S.Length - 1; i >= 0; i--)
+            {
+                if (S[i] == '>') { res[i] = Math.Max(res[i], index); index++; }
+                else { index = 1; }
+            }
+
+            long sum = 0;
+            sum = res.Sum();
+            /*
+            for (long i = 0; i < S.Length + 1; i++)
+            {
+                sum += res[i];
+            }
+             */
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
