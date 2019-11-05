0620b13a86e6484e544f3c7995f02766ab436182
布田遼平 <chittai1221@gmail.com>
2019-11-05 21:49:13 +0900

    Add B.cs & AC

---
+++ APC001/B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace APC001
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            long[] a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long[] b = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long res = 0;
+            for (long i = 0; i < N; i++)
+            {
+                res += Math.Max(0, (b[i] - a[i]) / 2) - Math.Max(0, a[i] - b[i]);
+            }
+            if (0 <= res) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
