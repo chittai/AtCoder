0b409fccbc54d8f2f66b478c912b805ffdb397a0
布田遼平 <chittai1221@gmail.com>
2019-11-06 23:38:10 +0900

    Add C.cs & AC

---
+++ ABC006/C.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ABC006
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            ulong[] a = new ulong[n + 1];
+            if (3 <= n) { a[2] = 1; }
+
+            for (int i = 3; i < n; i++)
+            {
+                a[i] = (a[i - 3] + a[i - 2] + a[i - 1]) % 10007;
+            }
+            Console.WriteLine(a[n - 1]);
+        }
+    }
+}
