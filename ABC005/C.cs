b9e903e15c14d4b45f83dd15cbefd8f6ad6d39e0
布田遼平 <chittai1221@gmail.com>
2019-11-09 00:37:51 +0900

    Add C.cs & AC

---
+++ ABC005/C.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC005
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            var A = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int M = int.Parse(Console.ReadLine());
+            var B = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            if (A.Count < B.Count) { Console.WriteLine("no"); return; }
+            else
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < B.Count; j++)
+                    {
+                        if (A[i] <= B[j] && B[j] <= A[i] + T)
+                        {
+                            B.RemoveAt(j); break;
+                        }
+                    }
+                }
+            }
+
+            if (B.Count == 0) Console.WriteLine("yes");
+            else Console.WriteLine("no");
+        }
+    }
+}
