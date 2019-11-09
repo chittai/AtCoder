bbec99c6c3cb470f9a46e9eec353e4e87541852e
布田遼平 <chittai1221@gmail.com>
2019-11-10 00:33:32 +0900

    Add A.cs & AC

---
+++ ZTP002/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ZTP002
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            if (N % 2 == 0) Console.WriteLine(N / 2 - 1);
+            else Console.WriteLine(N / 2);
+        }
+    }
+}
