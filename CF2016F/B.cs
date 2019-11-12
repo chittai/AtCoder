28feb6508bd5d9d8b5be8d12f45bdc15a18b4950
布田遼平 <chittai1221@gmail.com>
2019-11-12 22:27:24 +0900

    Add B.cs & AC

---
+++ CF2016F/B.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CF2016F
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            List<long> list = new List<long>();
+            long res = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                res += i;
+                list.Add(i);
+                if (N < res) break;
+            }
+
+            long sum = list.Sum();
+            list.Remove(sum - N);
+
+            foreach (var i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
