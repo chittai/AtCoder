6302ea100fb2461ad3194e59ac08cfd6200b9cb8
布田遼平 <chittai1221@gmail.com>
2019-11-12 21:32:07 +0900

    Add B.cs & AC

---
+++ CF2017qA/B.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace CF2017qA
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = input[0];
+            int M = input[1];
+            int K = input[2];
+
+            for (int i = 0; i <= N; i++)
+            {
+                for (int j = 0; j <= M; j++)
+                {
+                    if (i * (M - j) + j * (N - i) == K) { Console.WriteLine("Yes"); return; }
+                }
+            }
+            Console.WriteLine("No");
+        }
+    }
+}
