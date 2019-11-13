a6ac7c3af427be6a0b7c50d406a1f07a8f02ad33
布田遼平 <chittai1221@gmail.com>
2019-11-13 21:16:35 +0900

    Add C.cs & AC

---
+++ ARC068/C.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ARC068
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            long x = long.Parse(Console.ReadLine());
+
+            long a = x / 11;
+            long b = x % 11;
+
+            if (a == 0 && b <= 6) { Console.WriteLine(1); return; }
+            else if (a == 0 && 6 < b) { Console.WriteLine(2); return; }
+
+            if (0 < a && 0 <= b)
+            {
+                if (0 < b && b <= 6) { Console.WriteLine(2 * a + 1); return; }
+                else if (6 < b) { Console.WriteLine(2 * a + 2); return; }
+                else if (b == 0) { Console.WriteLine(2 * a); return; }
+            }
+        }
+    }
+}
