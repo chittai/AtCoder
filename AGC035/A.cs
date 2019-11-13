deb7767769230ae302f89fcff965c24bdc5f9483
布田遼平 <chittai1221@gmail.com>
2019-11-13 23:09:26 +0900

    Add A.cs & AC

---
+++ AGC035/A.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC035
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            long res = 0;
+            for (int i = 0; i < N; i++)
+            {
+                res ^= a[i];
+            }
+
+            if (res == 0) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
