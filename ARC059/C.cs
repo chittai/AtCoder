3c78e36e1b6a6d7868bf1a58890d7781f629c7ad
布田遼平 <chittai1221@gmail.com>
2019-11-15 00:51:27 +0900

    Add C.cs & AC

---
+++ ARC059/C.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ARC059
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = a.Sum();
+            int c = 0;
+            if (sum % N == 0) c = sum / N;
+            else if ((double)N / 2d < sum % N) c = sum / N + 1;
+            else c = sum / N;
+
+            double res = 0;
+            for (int i = 0; i < N; i++)
+            {
+                res += Math.Pow(a[i] - c, 2);
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
