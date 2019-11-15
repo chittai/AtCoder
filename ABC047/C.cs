6cd0b513dee65c7567ddd8df6f0971875d1bd14b
布田遼平 <chittai1221@gmail.com>
2019-11-15 23:39:01 +0900

    Add C.cs & AC

---
+++ ABC047/C.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ABC047
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            int count = 0;
+            for (int i = 0; i < S.Length - 1; i++)
+            {
+                if (S[i] != S[i + 1]) { count++; }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
