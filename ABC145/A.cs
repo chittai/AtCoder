f754b9b3aa6b2ded9a2e3399fa2ce6eb1bb1f691
布田遼平 <chittai1221@gmail.com>
2019-11-16 22:48:27 +0900

    Add A-B.cs & AC

---
+++ ABC145/A.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC145
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine());
+            double s1 = 1 * 1 * Math.PI;
+            double s2 = r * r * Math.PI;
+            Console.WriteLine(s2 / s1);
+        }
+    }
+}
---
+++ ABC145/B.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC145
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string S = Console.ReadLine();
+
+            if (S.Length % 2 != 0) { Console.WriteLine("No"); return; }
+
+            string s1 = S.Substring(0, S.Length / 2);
+            string s2 = S.Substring(S.Length / 2, S.Length / 2);
+
+            //Console.WriteLine(s1);
+            //Console.WriteLine(s2);
+
+            if (s1 == s2) { Console.WriteLine("Yes"); }
+            else { Console.WriteLine("No"); }
+
+        }
+    }
+}
