9072025168fee404b1a54ef9c8fc9f2ac71195f0
布田遼平 <chittai1221@gmail.com>
2019-11-19 00:26:33 +0900

    Add C.cs & AC

---
+++ ABC078/C.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long N = input[0];
+            long M = input[1];
+            Console.WriteLine(Math.Pow(2, M) * (1900 * M + 100 * (N - M)));
+        }
+    }
+}
