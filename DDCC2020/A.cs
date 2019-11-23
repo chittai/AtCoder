6b94c95679b64a74b96413ad70f46099f9e764d6
布田遼平 <chittai1221@gmail.com>
2019-11-23 22:58:55 +0900

    Add A-B.cs & AC

---
+++ DDCC2020/A.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace DDCC2020
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int X = input[0];
+            int Y = input[1];
+
+            int sum = 0;
+            if (X == 3) { sum += 100000; }
+            if (X == 2) { sum += 200000; }
+            if (X == 1) { sum += 300000; }
+
+            if (Y == 3) { sum += 100000; }
+            if (Y == 2) { sum += 200000; }
+            if (Y == 1) { sum += 300000; }
+
+
+            if (X == 1 && Y == 1) { sum += 400000; }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ DDCC2020/B.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace DDCC2020
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            long[] A = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long[] S = new long[N + 1];
+            for (int i = 0; i < N; i++)
+            {
+                S[i + 1] = S[i] + A[i];
+            }
+
+            long res = long.MaxValue;
+            for (int i = 0; i < N + 1; i++)
+            {
+                long L = S[i];
+                long R = S[N] - S[i];
+
+                res = Math.Min(res, Math.Abs(L - R));
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
