0a12b8235b9b31821de042fbb812c0cfe1fe3085
布田遼平 <chittai1221@gmail.com>
2019-11-25 23:44:29 +0900

    Add A.cs & AC

---
+++ ABC114/A.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ABC114
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+
+            if (X == 3 || X == 5 || X == 7) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
