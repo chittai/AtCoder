b577f9a12666c5b5c04d93fb2bbc62ecf84dce6d
布田遼平 <chittai1221@gmail.com>
2019-11-29 00:51:34 +0900

    Add C.cs & AC

---
+++ ARC006/C.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ARC006
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            List<long> list = new List<long>();
+            for (int i = 0; i < N; i++)
+            {
+                long w = long.Parse(Console.ReadLine());
+                long min = long.MaxValue;
+                int index = -1;
+
+                if (list.Count == 0) { list.Add(w); }
+                else
+                {
+                    for (int j = 0; j < list.Count; j++)
+                    {
+                        long m = list[j] - w;
+                        if (0 <= m && m < min)
+                        {
+                            index = j;
+                            min = m;
+                        }
+                    }
+                    if (-1 < index) list[index] = w;
+                    else list.Add(w);
+                }
+            }
+            //Console.WriteLine(string.Join(",", list));
+            Console.WriteLine(list.Count);
+        }
+    }
+}
