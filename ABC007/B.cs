15359e34179d4d68e22280f0b6b27053c213a52b
布田遼平 <chittai1221@gmail.com>
2019-11-28 21:24:03 +0900

    Add B.cs & AC

---
+++ ABC007/B.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABC007
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string A = Console.ReadLine();
+
+            if (A.Length == 1)
+            {
+                if (A[0] - 'a' == 0) { Console.WriteLine(-1); return; }
+                else { Console.WriteLine((char)(A[0] - 1)); return; }
+            }
+            Console.WriteLine(A.Substring(0, A.Length - 1));
+        }
+    }
+}
