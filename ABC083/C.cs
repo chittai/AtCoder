16be7afc6a755f4a6bbccee4dc1c8990de177912
布田遼平 <chittai1221@gmail.com>
2019-11-28 22:13:30 +0900

    Add C.cs & AC

---
+++ ABC083/C.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC083
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long X = input[0];
+            long Y = input[1];
+
+            long res = X;
+            int i = 0;
+            //while (X * Math.Pow(2, i) <= Y) i++;
+
+            while (res <= Y)
+            {
+                i++;
+                res *= 2;
+            }
+
+            Console.WriteLine(i);
+        }
+    }
+}
