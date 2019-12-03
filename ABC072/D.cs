6bb776343895b331dc9d3c7e04a4617ec416fb09
布田遼平 <chittai1221@gmail.com>
2019-12-04 00:15:44 +0900

    Add D.cs & AC

---
+++ ABC072/D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC072
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] P = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int res = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (P[i] == i + 1)
+                {
+                    if (i < N - 1)
+                    {
+                        int tmp = P[i + 1];
+                        P[i + 1] = P[i];
+                        P[i] = tmp;
+                    }
+                    res++;
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
