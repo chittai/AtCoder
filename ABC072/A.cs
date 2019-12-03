70e1a204277a53f1700c60866aca7e674bccee29
布田遼平 <chittai1221@gmail.com>
2019-12-04 00:15:35 +0900

    Add A,D.cs & AC

---
+++ ABC072/A.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC072
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int X = input[0];
+            int t = input[1];
+
+            if (X <= t) Console.WriteLine(0);
+            else Console.WriteLine(X - t);
+
+        }
+    }
+}
