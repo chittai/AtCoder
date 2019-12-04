e77f94461bbfa52300e633e8faabde4627d2d531
布田遼平 <chittai1221@gmail.com>
2019-12-05 00:02:59 +0900

    Modify A.cs & AC

--- AGC034/A.cs
+++ AGC034/A.cs
@@ -7,104 +7,42 @@ namespace AGC034
     {
         static void Main(string[] args)
         {
-            string[] s = Console.ReadLine().Split(' ');
-            int N = int.Parse(s[0]);
-            int A = int.Parse(s[1]);
-            int B = int.Parse(s[2]);
-            int C = int.Parse(s[3]);
-            int D = int.Parse(s[4]);
 
-            string s1 = Console.ReadLine();
-            /*
-                        if (s1[C - 1] == '#' || s1[D - 1] == '#')
-                        {
-                            Console.WriteLine("No");
-                            return;
-                        }
-             */
-            if (B < C && C < D)
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = input[0];
+            int A = input[1] - 1;
+            int B = input[2] - 1;
+            int C = input[3] - 1;
+            int D = input[4] - 1;
+            string s = Console.ReadLine();
+
+            if (C < D || C < B)
             {
-                var result1 = HaveTwoSharp(s1, A, C);
-                var result2 = HaveTwoSharp(s1, B, D);
-
-                if (!result1 && !result2)
+                int index = B;
+                int goal = D;
+                int count = 0;
+                while (count < 2)
                 {
-                    Console.WriteLine("Yes");
-                    return;
+                    while (index < goal)
+                    {
+                        if (index + 2 < s.Length && s[index + 2] == '.') { index += 2; }
+                        else if (index + 1 < s.Length && s[index + 1] == '.') { index += 1; }
+                        else { Console.WriteLine("No"); return; }
+                    }
+                    count++;
+                    index = A;
+                    goal = C;
                 }
-                else
-                {
-                    Console.WriteLine("No");
-                    return;
-                }
+                Console.WriteLine("Yes");
             }
-
-            if (C < B && C < D)
-            {
-                var result1 = HaveTwoSharp(s1, A, C);
-                var result2 = HaveTwoSharp(s1, B, D);
-
-                if (!result1 && !result2)
-                {
-                    Console.WriteLine("Yes");
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("No");
-                    return;
-                }
-            }
-
-            if (D < C)
-            {
-                var result1 = HaveTwoSharp(s1, A, C);
-                var result2 = HaveTwoSharp(s1, B, D);
-                var result3 = HaveThreeDot(s1, B, D);
-                //var result4 = HaveDotBD(s1, B);
-                //var result5 = HaveDotBD(s1, D);
-
-                if (!result1 && !result2 && result3)
-                {
-                    Console.WriteLine("Yes");
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("No");
-                    return;
-                }
-            }
-        }
-
-        static bool HaveTwoSharp(string s, int start, int end)
-        {
-            char[] c = s.Skip(start - 1).Take(end - start + 1).ToArray();
-            string s2 = new string(c);
-            return s2.Contains("##");
-        }
-
-        static bool HaveThreeDot(string s, int start, int end)
-        {
-            char[] c = s.Skip(start - 1).Take(end - start + 2).ToArray();
-            string s2 = new string(c);
-            return s2.Contains("...");
-        }
-
-        static bool HaveDotBD(string s, int end)
-        {
-            var c1 = s[end - 1];
-            var c2 = s[end - 2];
-            var c3 = s[end];
-
-            Console.WriteLine("c1" + c1);
-            Console.WriteLine("c2" + c2);
-            Console.WriteLine("c3" + c3);
-
-            if (c1 == '.' && c2 == '.' && c3 == '.')
-                return true;
             else
-                return false;
+            {
+                for (int i = B; i <= D; i++)
+                {
+                    if (i + 1 < s.Length && s[i - 1] == '.' && s[i] == '.' && s[i + 1] == '.') { Console.WriteLine("Yes"); return; }
+                }
+                Console.WriteLine("No");
+            }
         }
     }
 }
