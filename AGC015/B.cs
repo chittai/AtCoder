d0fa983e4894b9a195d7cc2e2804fdbb92b013c4
布田遼平 <chittai1221@gmail.com>
2019-12-06 22:53:38 +0900

    Add B.cs & AC

---
+++ AGC015/B.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC015
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            long res = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == 'U') { res += 2 * i + (S.Length - (i + 1)); }
+                else { res += 1 * i + (2 * (S.Length - (i + 1))); }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
