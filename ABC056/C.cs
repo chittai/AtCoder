fff8260873f6da7bcb8937ce966bdff419a138f8
布田遼平 <chittai1221@gmail.com>
2019-12-06 22:53:10 +0900

    Add C.cs & AC

---
+++ ABC056/C.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ABC056
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            long X = long.Parse(Console.ReadLine());
+
+            long sum = 0;
+            int t = 0;
+            for (int i = 1; ; i++)
+            {
+                t = i;
+                sum += i;
+                if (X <= sum) break;
+            }
+
+            Console.WriteLine(t);
+        }
+    }
+}
