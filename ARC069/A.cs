343c996f444cfc3be8236dd3fa769ad57fbed5f2
布田遼平 <chittai1221@gmail.com>
2019-12-07 23:04:54 +0900

    Add A.cs & AC

---
+++ ARC069/A.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ARC069
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long N = input[0];
+            long M = input[1];
+
+            long res = 0;
+            if (2 * N < M)
+            {
+                res = N + (M / 2 - N) / 2;
+            }
+            else
+            {
+                res = M / 2;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
