1a550a29b2766927cf60cd3f75685b0e945e6869
布田遼平 <chittai1221@gmail.com>
2019-12-08 22:48:35 +0900

    Add A-B.cs & AC

---
+++ ABC147/A.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC147
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (22 <= input.Sum()) Console.WriteLine("bust");
+            else Console.WriteLine("win");
+        }
+    }
+}
---
+++ ABC147/B.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC147
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            int res = 0;
+            for (int i = 0; i < S.Length / 2; i++)
+            {
+                if (S[i] != S[S.Length - 1 - i]) res++;
+            }
+
+            Console.WriteLine(res);
+
+        }
+    }
+}
