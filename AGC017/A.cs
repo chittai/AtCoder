8914ef430d5a2280e6dad8f85137c0068b19bbf3
布田遼平 <chittai1221@gmail.com>
2019-12-10 00:19:17 +0900

    Add A-A2.cs & AC

---
+++ AGC017/A.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC017
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long N = input[0];
+            long P = input[1];
+            long[] A = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long eCount = 0;
+            long oCount = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] % 2 == 0) eCount++;
+                else oCount++;
+            }
+            if (P == 1 && oCount == 0) { Console.WriteLine(0); return; }
+
+            double res = 0;
+            //res += Math.Pow(2, eCount); // 何も選ばないも含んでいる
+            long[,] c = new long[oCount + 5, oCount + 5];
+            c[0, 0] = 1;
+            for (int i = 1; i <= oCount; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == 0) c[i, j] = c[i - 1, j];
+                    else c[i, j] = c[i - 1, j] + c[i - 1, j - 1];
+                }
+            }
+
+            for (int i = 0; i <= oCount; i++)
+            {
+                if (i % 2 == P) res += Math.Pow(2, eCount) * c[oCount, i];
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ AGC017/A2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC017
+{
+    class A2
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long N = input[0];
+            long P = input[1];
+            long[] A = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long eCount = 0;
+            long oCount = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] % 2 == 0) eCount++;
+                else oCount++;
+            }
+
+            if (N == eCount && P == 1) Console.WriteLine(0);
+            else if (N == eCount && P == 0) Console.WriteLine(Math.Pow(2, N));
+            else Console.WriteLine(Math.Pow(2, N - 1));
+        }
+    }
+}
