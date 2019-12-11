6e421d692e531a58a14c4002ebdbd3392f35cccb
布田遼平 <chittai1221@gmail.com>
2019-12-12 00:56:39 +0900

    Add C.cs & AC

---
+++ ABC068/C.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC068
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = input[0];
+            int M = input[1];
+
+            bool[] S = new bool[200010];
+            bool[] G = new bool[200010];
+
+            for (int i = 0; i < M; i++)
+            {
+                input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int a = input[0];
+                int b = input[1];
+                if (a == 1) S[b] = true;
+                if (b == N) G[a] = true;
+            }
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] && G[i]) { Console.WriteLine("POSSIBLE"); return; }
+            }
+            Console.WriteLine("IMPOSSIBLE");
+        }
+    }
+}
