8f095ed774569cc4fc0b0d3896d8c5115573621d
布田遼平 <chittai1221@gmail.com>
2019-12-14 22:57:16 +0900

    Add A-A2.cs & AC

---
+++ AGC007/A.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC007
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int H = input[0];
+            int W = input[1];
+            char[,] A = new char[W, H];
+            for (int h = 0; h < H; h++)
+            {
+                string s = Console.ReadLine();
+                for (int w = 0; w < W; w++)
+                {
+                    A[w, h] = s[w];
+                }
+            }
+
+            for (int h = 0; h < H; h++)
+            {
+                for (int w = 0; w < W; w++)
+                {
+                    if (0 < w && 0 < h && A[w, h] == '#' && (A[w - 1, h] == '#' && A[w, h - 1] == '#'))
+                    {
+                        Console.WriteLine("Impossible");
+                        return;
+                    }
+
+                    if (w < W - 1 && h < H - 1 && A[w, h] == '#' && (A[w + 1, h] == '#' && A[w, h + 1] == '#'))
+                    {
+                        Console.WriteLine("Impossible");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("Possible");
+        }
+    }
+}
---
+++ AGC007/A2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC007
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int H = input[0];
+            int W = input[1];
+            sum = H + W - 1;
+            for (int h = 0; h < H; h++)
+            {
+                sum -= Console.ReadLine().ToCharArray().Count(x => x == '#');
+            }
+            if (sum == 0) Console.WriteLine("Possible");
+            else Console.WriteLine("Impossible");
+        }
+    }
+}
