48fed7cb798459ede1a186c85c77e5821e610e31
布田遼平 <chittai1221@gmail.com>
2019-12-15 21:38:03 +0900

    Add C.cs & AC

---
+++ ABC067/C.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC067
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            long[] a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long[] S = new long[N + 1];
+            for (long i = 0; i < N; i++)
+            {
+                S[i + 1] = S[i] + a[i];
+            }
+
+            long res = long.MaxValue;
+            for (long i = 1; i < N; i++)
+            {
+                long x = S[N] - S[i];
+                long y = S[i];
+                res = Math.Min(res, Math.Abs(x - y));
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
