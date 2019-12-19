6051bd6ad779bfcd4dc789f8f101cba4f5aeef1e
布田遼平 <chittai1221@gmail.com>
2019-12-19 16:40:24 +0900

    Add A.cs & AC

---
+++ AGC036/A.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AGC036
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            long W = 1000000000;
+            long S = long.Parse(Console.ReadLine());
+            long x = (W - S % W) % W;
+            long y = (S + x) / W;
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", 0, 0, W, 1, x, y);
+        }
+    }
+}
