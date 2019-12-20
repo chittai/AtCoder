0a3f5799ab3ba30b253796c6b6d4d49f4fea8e71
布田遼平 <chittai1221@gmail.com>
2019-12-21 00:37:25 +0900

    Add D.cs & AC

---
+++ ABC064/D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC064
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string S = Console.ReadLine();
+
+            int need_left = 0;
+            int need_right = 0;
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == '(') need_right++;
+                else
+                {
+                    if (0 < need_right) need_right--;
+                    else need_left++;
+                }
+            }
+
+            string res = "";
+            for (int i = 0; i < need_left; i++) res += '(';
+            res += S;
+            for (int i = 0; i < need_right; i++) res += ')';
+
+            Console.WriteLine(res);
+
+        }
+    }
+}
