cb9f6d8bab4649f259832b008c496a372000f7a1
布田遼平 <chittai1221@gmail.com>
2019-12-21 00:37:11 +0900

    Add A.cs & AC

---
+++ AGC005/A.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC005
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            int delete_count = 0;
+            int s = 0;
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == 'S') s++;
+                else
+                {
+                    if (0 < s) { delete_count++; s--; }
+                }
+            }
+
+            Console.WriteLine(S.Length - 2 * delete_count);
+        }
+    }
+}
