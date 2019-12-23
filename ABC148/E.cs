5cf46f0560bbf5eefdf68465609af5a0c7b08472
布田遼平 <chittai1221@gmail.com>
2019-12-23 22:28:49 +0900

    Add E.cs & AC

---
+++ ABC148/E.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC148
+{
+    class E
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            if (N % 2 != 0) { Console.WriteLine(0); return; }
+
+            long res = 0;
+            N /= 2;
+            while (5 <= N)
+            {
+                res += N / 5;
+                N /= 5;
+            }
+            Console.WriteLine(res);
+
+        }
+    }
+}
