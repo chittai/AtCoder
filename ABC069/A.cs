69e71227f29cb8aca260dcb45f25a51cad8e76f6
布田遼平 <chittai1221@gmail.com>
2019-12-24 23:34:23 +0900

    Add A.cs & AC

---
+++ ABC069/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC069
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine((input[0] - 1) * (input[1] - 1));
+        }
+    }
+}
