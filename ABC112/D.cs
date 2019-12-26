663f07eecd9e349e2c525cfb0c56559977f14d56
布田遼平 <chittai1221@gmail.com>
2019-12-27 00:15:00 +0900

    Add D.cs & AC

---
+++ ABC112/D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ABC112
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long N = input[0];
+            long M = input[1];
+
+            long res = 0;
+
+            for (long i = 1; i <= Math.Sqrt(M) + 1; i++)
+            {
+
+                if (M % i == 0)
+                {
+                    if (i <= M / N) res = Math.Max(res, i);
+                    if (M / i <= M / N) res = Math.Max(res, M / i);
+                }
+
+            }
+            Console.WriteLine(res);
+
+        }
+    }
+}
