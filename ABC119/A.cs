a206dd78b5edacdca69ee028022678ecdd0cd8e8
布田遼平 <chittai1221@gmail.com>
2019-12-27 23:51:30 +0900

    Add A.cs & AC

---
+++ ABC119/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ABC119
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            if (0 < s.CompareTo("2019/04/30")) Console.WriteLine("TBD");
+            else Console.WriteLine("Heisei");
+        }
+    }
+}
