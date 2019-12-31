63558892ed873dad4c6986a69c2242d2a82d8535
布田遼平 <chittai1221@gmail.com>
2019-12-31 16:24:54 +0900

    Add A.cs & AC

---
+++ AGC023/A.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace AGC023
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            long[] A = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long[] S = new long[N + 1];
+            for (int i = 0; i < N; i++)
+            {
+                S[i + 1] = S[i] + A[i];
+            }
+
+            S = S.OrderBy(x => x).ToArray();
+            long res = 0;
+            for (int i = 0; i < N; i++)
+            {
+                long count = 1;
+                while (i < N && S[i] == S[i + 1])
+                {
+                    count++;
+                    i++;
+                }
+
+                if (1 < count) res += count * (count - 1) / 2;
+
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
