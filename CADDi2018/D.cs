97621e01495787df292024df25beeca145cff645
布田遼平 <chittai1221@gmail.com>
2020-01-06 22:38:17 +0900

    Add D.cs & AC

---
+++ CADDi2018/D.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CADDi2018
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] a = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            if (0 < a.Count(x => x % 2 == 1)) Console.WriteLine("first");
+            else Console.WriteLine("second");
+        }
+    }
+}
