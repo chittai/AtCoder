e29e5ac59113476c25f3fa638a1bf841c1147f0f
布田遼平 <chittai1221@gmail.com>
2020-01-07 22:42:40 +0900

    Add C.cs & AC

---
+++ T1PC2017/C.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T1PC2017
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            for (int h = 1; h <= 3500; h++)
+            {
+                for (int n = 1; n <= 3500; n++)
+                {
+                    if (4 * h * n - N * n - N * h > 0)
+                    {
+                        if ((N * h * n) % (4 * h * n - N * n - N * h) == 0)
+                        {
+                            Console.WriteLine("{0} {1} {2}", h, n, (N * h * n) / (4 * h * n - N * n - N * h));
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
