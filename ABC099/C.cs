61e190640c5ab187e25ca666d90e7db80ee5e759
布田遼平 <chittai1221@gmail.com>
2020-01-08 23:34:19 +0900

    Add C.cs & AC

---
+++ ABC099/C.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC099
+{
+    class C
+    {
+        static int[] memo;
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            memo = new int[N + 1000].Select(x => -1).ToArray();
+
+            Console.WriteLine(rec(N));
+        }
+
+        static int rec(int n)
+        {
+            if (n == 0) return 0;
+            if (memo[n] != -1) return memo[n];
+
+            int res = n;
+            for (int i = 1; i <= n; i *= 6) res = Math.Min(res, rec(n - i) + 1);
+            for (int i = 1; i <= n; i *= 9) res = Math.Min(res, rec(n - i) + 1);
+
+            memo[n] = res;
+            return memo[n];
+        }
+    }
+}
