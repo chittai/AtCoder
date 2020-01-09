65f179d6bfe441145cf2062d6353dbfbdb47745c
布田遼平 <chittai1221@gmail.com>
2020-01-09 22:37:09 +0900

    Modify C.cs & AC

--- ARC097/C.cs
+++ ARC097/C.cs
@@ -12,43 +12,19 @@ namespace ARC097
             string s = Console.ReadLine();
             int K = int.Parse(Console.ReadLine());
 
-            //a-z
-            int[] alpha = new int[26];
-            for (int i = 0; i < s.Length; i++)
+            List<string> list = new List<string>();
+            for (int i = 1; i <= 5; i++)
             {
-                alpha[s[i] - 'a']++;
-            }
-
-            string res = "";
-            if (K <= alpha.Count(x => 0 < x))
-            {
-                int count = 0;
-                for (char c = 'a'; c <= 'z'; c++)
+                for (int j = 0; j < s.Length - i + 1; j++)
                 {
-                    if (0 < alpha[c - 'a'])
-                    {
-                        count++;
-                        if (count == K) res = c.ToString();
-                    }
+                    string tmp = s.Substring(j, i);
+                    list.Add(tmp);
                 }
-            }
-            else
-            {
-                int rank = K - alpha.Count(x => 0 < x);
-                List<string> list = new List<string>();
-                for (int i = 2; i <= 5; i++)
-                {
-                    for (int j = 0; j < s.Length - i + 1; j++)
-                    {
-                        string tmp = s.Substring(j, i);
-                        list.Add(tmp);
-                    }
 
-                    list = list.OrderBy(x => x).ToList();
-                    if (rank <= list.Count) { res = list[rank - 1]; break; }
-                }
+                list = list.Distinct().OrderBy(x => x).ToList();
             }
-            Console.WriteLine(res);
+            //Console.WriteLine(string.Join(",", list));
+            Console.WriteLine(list[K - 1]);
         }
     }
 }
