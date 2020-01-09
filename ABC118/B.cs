5b0dd3d4e6c02aa2096119d766adfdd14bfebb5e
布田遼平 <chittai1221@gmail.com>
2020-01-09 23:04:43 +0900

    Add B.cs & AC

---
+++ ABC118/B.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC118
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int N = input[0];
+            int M = input[1];
+
+            int[] Ans = new int[M];
+            for (int i = 0; i < N; i++)
+            {
+                input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int K = input[0];
+                for (int j = 1; j <= K; j++)
+                {
+                    Ans[input[j] - 1]++;
+                }
+            }
+
+            int res = Ans.Count(x => x == N);
+            Console.WriteLine(res);
+        }
+    }
+}
