c0a42727b2280537ecc5ed776c57e8b68c327735
taquan <chittai1221@gmail.com>
2020-01-12 00:54:32 +0900

    Add A,A2.py & AC

---
+++ ABC115/A.py
@@ -0,0 +1,12 @@
+D = int(input())
+s = ""
+if D == 25:
+    s = 'Christmas'
+elif D == 24:
+    s = 'Christmas Eve'
+elif D == 23:
+    s = 'Christmas Eve Eve'
+elif D == 22:
+    s = 'Christmas Eve Eve Eve'
+
+print(s)
