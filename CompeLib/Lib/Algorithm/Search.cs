50cabdf7343d009ab60133785f2d10ce955738c7
布田遼平 <chittai1221@gmail.com>
2020-01-13 00:59:29 +0900

    Modify Search.cs & Add Method

--- CompeLib/Lib/Algorithm/Search.cs
+++ CompeLib/Lib/Algorithm/Search.cs
@@ -5,6 +5,16 @@ using System.Linq;
 namespace Algorithm
 {
 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CountStepByBFS
+
+
+        }
+    }
+
     class BinarySearch
     {
         public static int LowerBound<T>(T[] a, T v)
@@ -138,7 +148,52 @@ namespace Algorithm
 
     class BFS
     {
-        public static void bfs()
+        /// <summary>
+        /// (sx, sy) -> (gx, gy)の最短経路を検索する。dist にホップ数を格納する
+        /// </summary>
+        static int[,] map;
+        static int X;
+        static int Y;
+        static int dist;
+        static void CountStepByBFS(int sx, int sy, int gx, int gy)
+        {
+            Queue<Tuple<int, int, int>> tq = new Queue<Tuple<int, int, int>>();
+            tq.Enqueue(Tuple.Create(sx, sy, 0));
+            int step = 0;
+            map[sx, sy] = '#';
+
+            int[] vx = { 0, 1, 0, -1 };
+            int[] vy = { 1, 0, -1, 0 };
+
+            while (0 < tq.Count)
+            {
+                var q = tq.Dequeue();
+                int x = q.Item1;
+                int y = q.Item2;
+                step = q.Item3;
+
+                if (x == gx && y == gy)
+                {
+                    dist = step;
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    int nx = x + vx[i];
+                    int ny = y + vy[i];
+
+                    if ((0 <= nx && nx < X) && (0 <= ny && ny < Y) && map[nx, ny] == '.')
+                    {
+                        map[nx, ny] = '#';
+                        tq.Enqueue(Tuple.Create(nx, ny, step + 1));
+                    }
+                }
+            }
+        }
+
+
+        /* 
+        public static void BFS()
         {
             int N = 10;
             List<int>[] list = new List<int>[N];
@@ -185,5 +240,6 @@ namespace Algorithm
                 }
             }
         }
+        */
     }
 }
