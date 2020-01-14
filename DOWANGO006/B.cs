0621688d912c62b1b20abbcc48c8c3199edfadac
布田遼平 <chittai1221@gmail.com>
2020-01-15 01:15:51 +0900

    Add B.cs & WIP

---
+++ DOWANGO006/B.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace DOWANGO006
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            long[] x = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long[] dist = new long[N - 1];
+            for (int i = 0; i < N - 1; i++)
+            {
+                dist[i] = x[i + 1] - x[i];
+            }
+
+            long res = 0;
+            for (int i = 0; i < N - 1; i++)
+            {
+                res += dist[i] 
+
+            }
+            Console.WriteLine(string.Join(",", dist));
+            Console.WriteLine(res);
+        }
+    }
+}
