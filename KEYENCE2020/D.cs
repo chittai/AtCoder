3c91e12f4290224347a72c62b6ac7a7ff71e414b
布田遼平 <chittai1221@gmail.com>
2020-01-18 22:58:31 +0900

    Add D.cs & WIP

---
+++ KEYENCE2020/D.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace KEYENCE2020
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] A = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] B = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] Odd = new int[N];
+            int[] Even = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Odd[i] = A[i];
+                    Even[i] = B[i];
+                }
+                else
+                {
+                    Odd[i] = B[i];
+                    Even[i] = A[i];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", Odd));
+            Console.WriteLine(string.Join(" ", Even));
+
+        }
+    }
+}
