992ca63b991fc03b89110217175c86a78ce162db
布田遼平 <chittai1221@gmail.com>
2020-01-19 23:47:06 +0900

    Add A-C.cs & AC

---
+++ ABC152/A.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC152
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (input[0] == input[1]) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+
+        }
+    }
+}
---
+++ ABC152/B.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC152
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = input[0];
+            int b = input[1];
+
+            if (a <= b)
+            {
+                for (int i = 0; i < (int)b; i++)
+                {
+                    Console.Write(a);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < (int)a; i++)
+                {
+                    Console.Write(b);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
