d28233a0d68f0709907370dd74701a35612e2b7f
布田遼平 <chittai1221@gmail.com>
2020-01-19 23:47:19 +0900

    Add D-E.cs & WIP

---
+++ ABC152/D.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC152
+{
+    class D
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] P = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int min = int.MaxValue;
+            int res = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (P[i] <= min) { res++; }
+                min = Math.Min(min, P[i]);
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ ABC152/E.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC152
+{
+    class E
+    {
+        static ulong X = 1000000007;
+        static void Main(string[] args)
+        {
+            ulong N = ulong.Parse(Console.ReadLine());
+            ulong[] A = Console.ReadLine().Split().Select(ulong.Parse).ToArray();
+            ulong lcm = A[0];
+            ulong tes = 0;
+            for (ulong i = 1; i < N; i++)
+            {
+                lcm = CalcLCM(lcm, A[i]);
+                tes += lcm / A[i] % X;
+            }
+            ulong res = 0;
+            for (ulong i = 0; i < N; i++)
+            {
+                res += (lcm / A[i]) % X;
+            }
+
+            Console.WriteLine(lcm);
+            Console.WriteLine(tes);
+            Console.WriteLine(res % X);
+        }
+
+        public static ulong CalcGCD(ulong a, ulong b)
+        {
+            if (b == 0) { return a; }
+            return CalcGCD(b, a % b);
+        }
+
+        public static ulong CalcLCM(ulong a, ulong b)
+        {
+            return a / CalcGCD(a, b) * b % X;
+        }
+
+    }
+}
