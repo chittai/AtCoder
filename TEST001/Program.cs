ce07f17b2289f5cd33390efcf58893f71b702692
布田遼平 <chittai1221@gmail.com>
2020-01-21 22:52:33 +0900

    Add TEST

---
+++ TEST001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace TEST001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine((char)(input[0] + '0'));
+
+
+
+        }
+    }
+}
