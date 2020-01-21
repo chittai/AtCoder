112f7923efada516c71cb534a14b0ec306313512
布田遼平 <chittai1221@gmail.com>
2020-01-21 22:52:40 +0900

    Add A.cs & AC

---
+++ ABC030/A.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC030
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0];
+            int m = input[1];
+            var degree = Math.Abs((6 * m) % 360 - (30 * n + 0.5 * m) % 360);
+            Console.WriteLine(Math.Min(360 - degree, degree));
+        }
+    }
+}
