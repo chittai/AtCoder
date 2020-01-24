2b64ed9f6a3d865a64ed4acf4161862b5c4dfcc5
布田遼平 <chittai1221@gmail.com>
2020-01-25 00:18:19 +0900

    Add A.cs & AC

---
+++ ARC043/A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ARC043
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            int N = (int)input[0];
+            double A = input[1];
+            double B = input[2];
+            double[] S = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                S[i] = int.Parse(Console.ReadLine());
+            }
+
+            if (S.Max() - S.Min() != 0)
+            {
+                double p = B / (S.Max() - S.Min());
+                double q = A - p * S.Sum() / N;
+                Console.WriteLine("{0} {1}", p, q);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
