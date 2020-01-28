c84c665de438ac8e5205e9078b7436986d69b70d
布田遼平 <chittai1221@gmail.com>
2020-01-28 22:11:11 +0900

    Modify E.cs & AC

--- ABC153/E.cs
+++ ABC153/E.cs
@@ -5,20 +5,15 @@ namespace ABC153
 {
     class E
     {
-        static long H;
-        static long N;
-        static long[,] DP;
-        static long[] A;
-        static long[] B;
         static void Main(string[] args)
         {
             long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
-            H = input[0];
-            N = input[1];
+            long H = input[0];
+            long N = input[1];
 
-            DP = new long[N + 10, H + 10];
-            A = new long[N];
-            B = new long[N];
+            long[,] DP = new long[N + 10, H + 10];
+            long[] A = new long[N];
+            long[] B = new long[N];
 
             for (int i = 0; i < N; i++)
             {
@@ -27,53 +22,24 @@ namespace ABC153
                 B[i] = input[1];
             }
 
-            /*
-            for (int i = 0; i < N + 10; i++)
-            {
-                for (int j = 0; j < H + 10; j++)
-                {
-                    DP[i, j] = 0;
-                }
-            }
-            */
-
-            for (int h = 0; h <= H; h++)
-            {
-                DP[0, h] = 0;
-            }
-
-            for (int h = 0; h <= H; h++)
-            {
-                if (h % A[1] == 0) DP[1, h] = h / A[1] * B[1];
-                //else DP[1, h] = h / A[1] * B[1] + B[1];
-            }
-
-            for (int i = 1; i < N; i++)
+            for (int i = 0; i <= N; i++)
             {
                 for (int h = 0; h <= H; h++)
                 {
-                    if (h >= A[i] * B[i])
-                    {
-                        DP[i + 1, h] = Math.Min(DP[i, h - H / A[i] * B[i]] + H / A[i] * B[i], DP[i, h]);
-                    }
-                    else
-                    {
-                        DP[i + 1, h] = DP[i, h];
-                    }
+                    DP[i, h] = int.MaxValue;
                 }
             }
 
-            Console.WriteLine(DP[N, H - 1]);
-
-
-            for (int i = 0; i < N + 10; i++)
+            DP[0, 0] = 0;
+            for (int i = 0; i < N; i++)
             {
-                for (int j = 0; j < H + 10; j++)
+                for (int h = 0; h <= H; h++)
                 {
-                    if (DP[i, j] != 0 && DP[i, j] != int.MaxValue) Console.WriteLine(DP[i, j]);
+                    DP[i + 1, h] = Math.Min(DP[i + 1, h], DP[i, h]);
+                    DP[i + 1, Math.Min(h + A[i], H)] = Math.Min(DP[i + 1, Math.Min(h + A[i], H)], DP[i + 1, h] + B[i]);
                 }
             }
-
+            Console.WriteLine(DP[N, H]);
         }
     }
 }
