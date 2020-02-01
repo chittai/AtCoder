8b82b1f708f3bee1d01c47bf817f0774951e4e2d
布田遼平 <chittai1221@gmail.com>
2020-02-01 23:56:22 +0900

    Add A.cs & WIP

---
+++ AGC037/A.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AGC037
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            int res = 0;
+            string temp = S[0].ToString();
+            for (int i = 1; i < S.Length - 1; i++)
+            {
+                int len = temp.Length;
+                while (0 < len)
+                {
+
+                }
+
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
