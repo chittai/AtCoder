d4b7d088fdcff618de94cee3fe4b0d461385c6f1
布田遼平 <chittai1221@gmail.com>
2020-02-11 23:54:31 +0900

    Modify E.cs & AC

--- ABC154/E.cs
+++ ABC154/E.cs
@@ -10,28 +10,34 @@ namespace ABC154
             string N = Console.ReadLine();
             int K = int.Parse(Console.ReadLine());
 
-            long res = 0;
-            if (K == 1)
+            int[,,] dp = new int[105, 4, 2];
+            dp[0, 0, 0] = 1;
+
+            for (int i = 0; i < N.Length; i++)
             {
-                res += N[0] - '0';
-                res += 9 * (N.Length - 1);
-            }
-            else if (K == 2)
-            {
-                for (int i = 0; i < N.Length - 1; i++)
+                for (int j = 0; j < 4; j++)
                 {
-                    for (int j = i + 1; j < N.Length; j++)
+                    for (int k = 0; k < 2; k++)
                     {
-
+                        int nd = N[i] - '0';
+                        for (int d = 0; d < 10; d++)
+                        {
+                            int ni = i + 1;
+                            int nj = j;
+                            int nk = k;
+                            if (d != 0) nj++;
+                            if (nj > K) continue;
+                            if (k == 0)
+                            {
+                                if (d > nd) continue;
+                                if (d < nd) nk = 1;
+                            }
+                            dp[ni, nj, nk] += dp[i, j, k];
+                        }
                     }
-
                 }
             }
-
-
-
-            Console.WriteLine(res);
-
+            Console.WriteLine(dp[N.Length, K, 0] + dp[N.Length, K, 1]);
         }
     }
 }
