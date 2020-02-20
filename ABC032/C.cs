2bb916846f844bda7b3c744a7615a1b1fa8aeada
布田遼平 <chittai1221@gmail.com>
2020-02-21 00:39:25 +0900

    Add C.cs & AC

---
+++ ABC032/C.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC032
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long N = input[0];
+            long K = input[1];
+            long[] s = new long[N];
+            for (int i = 0; i < N; i++)
+            {
+                s[i] = long.Parse(Console.ReadLine());
+            }
+
+            long res = 0;
+            long right = 0;
+            long x = 1;
+
+            if (s.Contains(0)) { Console.WriteLine(N); return; }
+
+
+            for (int left = 0; left < N; left++)
+            {
+                while (right < N && x * s[right] <= K)
+                {
+                    x *= s[right];
+                    right++;
+                }
+
+                res = Math.Max(res, right - left);
+
+                if (right == left) right++;
+                else if (s[left] == 0) { x = 0; res = left + 1; }
+                else x /= s[left];
+
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
