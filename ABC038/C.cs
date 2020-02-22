a163258cfc2cd7283e69684c3450dc1eeafcf44e
布田遼平 <chittai1221@gmail.com>
2020-02-22 10:58:42 +0900

    Add C.cs & AC

---
+++ ABC038/C.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC038
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            long[] a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long res = 0;
+            long right = 0;
+            for (int left = 0; left < N; left++)
+            {
+                while (right < N - 1 && a[right] < a[right + 1])
+                {
+                    right++;
+                }
+
+                res += right - left;
+
+                if (right == left) right++;
+            }
+            Console.WriteLine(res + N);
+        }
+    }
+}
