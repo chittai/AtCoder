7e00f4f69f1ec36785a5840686bdc3cd77b3f7e3
布田遼平 <chittai1221@gmail.com>
2020-02-24 23:01:09 +0900

    Add B.cs & 部分点

---
+++ ARC022/B.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ARC022
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            long N = long.Parse(Console.ReadLine());
+            long[] A = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            long right = 0;
+            long res = 0;
+            List<long> list = new List<long>();
+
+            for (int left = 0; left < N; left++)
+            {
+                while (right < N && !list.Contains(A[right]))
+                {
+                    list.Add(A[right]);
+                    right++;
+                }
+
+                res = Math.Max(res, right - left);
+
+                if (right == left) right++;
+                else list.RemoveAt(0);
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
