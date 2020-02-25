08a0fcadb2a2b8ee34d04ce3e7707b8081887511
布田遼平 <chittai1221@gmail.com>
2020-02-26 03:08:16 +0900

    Modify D.cs & AC

--- ABC156/D.cs
+++ ABC156/D.cs
@@ -12,26 +12,51 @@ namespace ABC156
             long b = input[2];
             long mod = 1000000007;
 
-            double ares = 1;
-            for (double i = 1; i <= a; i++)
-            {
-                ares *= (n - i + 1) / i % mod;
-            }
-
-            double bres = 1;
-            for (double i = 1; i <= b; i++)
-            {
-                bres *= (n - i + 1) / i % mod;
-            }
-
-            long res = 1;
-            for (int i = 0; i < n; i++)
-            {
-                res = (res * 2) % mod;
-            }
-
-            Console.WriteLine(res - 1 - (long)ares - (long)bres);
-
+            long total = modPow(2, n, mod) - 1;
+            long ca = modCombination(n, a, mod) % mod;
+            long cb = modCombination(n, b, mod) % mod;
+            Console.WriteLine((total + mod - (ca + cb) % mod) % mod);
         }
+
+        public static long modPow(long a, long n, long mod)
+        {
+            long res = 1;
+            while (n > 0)
+            {
+                if ((n & 1) > 0) res = res * a % mod;
+                a = a * a % mod;
+                n >>= 1;
+            }
+            return res % mod;
+        }
+        public static long modInv(long a, long mod)
+        {
+            return modPow(a, mod - 2, mod) % mod;
+        }
+
+        public static long modMul(long a, long b, long mod)
+        {
+            return ((a % mod) * (b % mod)) % mod;
+        }
+
+        public static long modDiv(long a, long b, long mod)
+        {
+            return modMul(a, modInv(b, mod), mod);
+        }
+
+        private static long modCombination(long n, long k, long mod)
+        {
+            long answer = 1;
+            for (long i = n; i > n - k; i--)
+            {
+                answer = modMul(answer, i, mod);
+            }
+            for (long i = 1; i <= k; i++)
+            {
+                answer = modDiv(answer, i, mod);
+            }
+            return answer;
+        }
+
     }
 }
