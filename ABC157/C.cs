e512f28881f2b8262895edcb8b71691ebca14c8b
布田遼平 <chittai1221@gmail.com>
2020-03-02 00:55:52 +0900

    Modify C.cs & AC

--- ABC157/C.cs
+++ ABC157/C.cs
@@ -8,42 +8,20 @@ namespace ABC157
     {
         static void Main(string[] args)
         {
-            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int N = input[0];
-            int M = input[1];
-            int[] s = new int[M];
-            int[] c = new int[M];
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = input[0];
+            var M = input[1];
+            var sc = Enumerable.Repeat(0, M).Select(_ => Console.ReadLine().Split().Select(int.Parse).ToArray()).ToArray();
 
-            if (1 < N && M == 0) { Console.WriteLine(-1); return; }
-            if (N == 1 && M == 0) { Console.WriteLine(0); return; }
-
-            for (int i = 0; i < M; i++)
+            for (int i = 0; i < 1000; i++)
             {
-                input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                s[i] = input[0];
-                c[i] = input[1];
-                if (1 < N && s[i] == 1 && c[i] == 0) { Console.WriteLine(-1); return; }
-
+                var s = i.ToString();
+                if (s.Length != N) continue;
+                if (!sc.All(x => s[x[0] - 1] - '0' == x[1])) continue;
+                Console.WriteLine(i);
+                return;
             }
-
-            int[] res = new int[N].Select(x => x = int.MaxValue).ToArray();
-            for (int i = 0; i < M; i++)
-            {
-                if (res[s[i] - 1] != int.MaxValue && res[s[i] - 1] != c[i]) { Console.WriteLine(-1); return; }
-                if (c[i] < res[s[i] - 1]) res[s[i] - 1] = c[i];
-            }
-
-            for (int i = 0; i < N; i++)
-            {
-                if (res[0] == int.MaxValue) res[0] = 1;
-                if (res[i] == int.MaxValue) res[i] = 0;
-            }
-
-            for (int i = 0; i < N; i++)
-            {
-                Console.Write(res[i]);
-            }
-            Console.WriteLine();
+            Console.WriteLine(-1);
         }
     }
 }
