a30abd7bd37080c14fed740ea7bb387ab2c54735
布田遼平 <chittai1221@gmail.com>
2020-03-05 01:05:57 +0900

    Add A-B.cs & AC

---
+++ ABC110/A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC110
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            var h = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToArray();
+            Console.WriteLine(h[0] * 10 + h[1] + h[2]);
+        }
+    }
+}
---
+++ ABC110/B.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC110
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            var NMXY = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var xmax = Console.ReadLine().Split().Select(int.Parse).ToArray().Max();
+            var ymin = Console.ReadLine().Split().Select(int.Parse).ToArray().Min();
+
+            if (NMXY[2] < NMXY[3] && Math.Max(NMXY[2], xmax) < Math.Min(NMXY[3], ymin)) Console.WriteLine("No War");
+            else Console.WriteLine("War");
+        }
+    }
+}
