c5ed75ffd0756a7416aae8267099d462fc4477d2
布田遼平 <chittai1221@gmail.com>
2020-03-14 03:58:12 +0900

    Add C.cs & AC

---
+++ ABC105/C.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC105
+{
+    class C
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+
+            var ans = "";
+            if (N == 0) ans = "0";
+            while (N != 0)
+            {
+                if (N % 2 != 0)
+                {
+                    N--;
+                    ans = "1" + ans;
+                }
+                else
+                {
+                    ans = "0" + ans;
+                }
+                N /= -2;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
