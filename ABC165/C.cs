184587c1080d599956f6bae3bdc8e23c280ff88f
布田遼平 <chittai1221@gmail.com>
2020-05-07 00:29:21 +0900

    Modify C.cs & AC

--- ABC165/C.cs
+++ ABC165/C.cs
@@ -9,44 +9,40 @@ namespace ABC165
         static long N;
         static long M;
         static long Q;
+        static long[][] abcd;
+        static long ans;
         static void Main(string[] args)
         {
             //var X = long.Parse(Console.ReadLine());
             var NMQ = Console.ReadLine().Split().Select(long.Parse).ToArray();
             N = NMQ[0]; M = NMQ[1]; Q = NMQ[2];
-            var abcd = Enumerable.Repeat(0, (int)Q).Select(_ => Console.ReadLine().Split().Select(long.Parse).ToArray()).ToArray();
+            abcd = Enumerable.Repeat(0, (int)Q).Select(_ => Console.ReadLine().Split().Select(long.Parse).ToArray()).ToArray();
 
-            long ans = 0;
-            for (int bit = 0; bit < (1 << 10); bit++)
-            {
-                var list = new List<int>();
-                for (int i = 0; i < M; i++)
-                {
-                    if ((bit & (1 << i)) > 0) list.Add(i + 1);
-                }
-                if (list.Count != N) continue;
-                Console.WriteLine(string.Join(" ", list));
-                long sum = 0;
-                for (int q = 0; q < Q; q++)
-                {
-                    //Console.WriteLine(list[(int)abcd[q][1] - 1]);
-                    if (list[(int)abcd[q][1] - 1] - list[(int)abcd[q][0] - 1] == abcd[q][2]) { sum += abcd[q][3]; }
-                }
-                ans = Math.Max(ans, sum);
-                //if (ans == 219802) { Console.WriteLine(string.Join(" ", list)); break; };
+            var list = new List<long>();
+            DFS(1, list, N);
 
-            }
             Console.WriteLine(ans);
         }
 
-        static void DFS(int len)
+        static void DFS(int min, List<long> list, long len)
         {
-            if (len == N)
+            if (len == 0)
             {
-
+                long sum = 0;
+                for (int q = 0; q < Q; q++)
+                {
+                    if (list[(int)abcd[q][1] - 1] - list[(int)abcd[q][0] - 1] == abcd[q][2]) sum += abcd[q][3];
+                }
+                ans = Math.Max(ans, sum);
+                return;
             }
 
+            for (int i = min; i <= M; i++)
+            {
+                list.Add(i);
+                DFS(i, list, len - 1);
+                list.RemoveAt(list.Count - 1);
+            }
         }
-
     }
 }
