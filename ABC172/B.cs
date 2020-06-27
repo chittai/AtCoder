d177d5eb41ed32dc2026a1283420dde7cf3bb8d2
布田遼平 <chittai1221@gmail.com>
2020-06-27 22:30:07 +0900

    Add A-B,D.cs & AC

---
+++ ABC172/B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC172
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            var S = Console.ReadLine();
+            var T = Console.ReadLine();
+
+            var ans = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] != T[i]) ans++;
+            }
+
+            Console.WriteLine(ans);
+
+        }
+    }
+}
