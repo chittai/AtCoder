c8b8af5e9a12f47d7be54ad3bd5368a2b41c91ea
布田遼平 <chittai1221@gmail.com>
2020-07-11 23:51:55 +0900

    Modify C.cs & AC

--- AIPC2020/C.cs
+++ AIPC2020/C.cs
@@ -12,31 +12,22 @@ namespace AIPC2020
             //var LRd = Console.ReadLine().Split().Select(long.Parse).ToArray();
             var N = long.Parse(Console.ReadLine());
             //var a = Console.ReadLine().Split().Select(long.Parse).ToArray();
-
-
+            var n = new int[N + 10];
+            for (long x = 1; x <= 100; x++)
+            {
+                for (long y = 1; y <= 100; y++)
+                {
+                    for (long z = 1; z <= 100; z++)
+                    {
+                        var sum = Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2) + (x * y) + (y * z) + (z * x);
+                        if (sum <= N) n[(long)sum]++;
+                    }
+                }
+            }
             for (int i = 1; i <= N; i++)
             {
-                var ans = 0;
-                for (long x = 1; x <= 100; x++)
-                {
-                    //double sum = Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2);
-                    for (long y = x; y <= 100; y++)
-                    {
-                        z = 
-                        double sum = Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2);
-                            if (sum == N) ans++;
-
-                        }
-                    }
-                    Console.WriteLine(ans);
-                }
-
-                Console.WriteLine(ans);
+                Console.WriteLine(n[i]);
             }
-
-
-
-            Console.WriteLine(ans);
         }
     }
 }
