618cce3b9525528c3470112c9f81893ff57583f5
布田遼平 <chittai1221@gmail.com>
2020-07-11 22:30:10 +0900

    Add A-B.cs & AC

---
+++ AIPC2020/A.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AIPC2020
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            //var CA = Enumerable.Repeat(0, (int)N).Select(_ => Console.ReadLine().Split().Select(long.Parse).ToArray()).ToArray();
+            var LRd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            //var N = long.Parse(Console.ReadLine());
+
+            var ans = 0;
+            for (int i = LRd[0]; i <= LRd[1]; i++)
+            {
+                if (i % LRd[2] == 0) ans++;
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ AIPC2020/B.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AIPC2020
+{
+    class B
+    {
+        static void Main(string[] args)
+        {
+            //var CA = Enumerable.Repeat(0, (int)N).Select(_ => Console.ReadLine().Split().Select(long.Parse).ToArray()).ToArray();
+            //var LRd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var N = int.Parse(Console.ReadLine());
+            var a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var ans = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (i % 2 == 0 && a[i] % 2 == 1) ans++;
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
