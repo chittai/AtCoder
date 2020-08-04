0c0da41448e61ad755cc03826ca8008f5dae1923
布田遼平 <chittai1221@gmail.com>
2020-08-05 02:56:01 +0900

    Add C,E.cs & AC

---
+++ ABC174/C.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ABC174
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var NK = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            //var p = Console.ReadLine().Split().Select(long.Parse).OrderBy(x => x).ToArray();
+            // var CA = Enumerable.Repeat(0, (int)N).Select(_ => Console.ReadLine().Split().Select(long.Parse).ToArray()).ToArray();
+            var K = long.Parse(Console.ReadLine());
+            var x = 7L;
+            for (long i = 0; i <= K; i++)
+            {
+                if (x % K == 0) { Console.WriteLine(i + 1); return; }
+                x = (10 * x + 7) % K;
+            }
+            Console.WriteLine(-1);
+        }
+    }
+}
---
+++ ABC174/E.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ABC174
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var NK = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            var N = NK[0]; var K = NK[1];
+            var A = Console.ReadLine().Split().Select(long.Parse).OrderBy(x => x).ToArray();
+            // var CA = Enumerable.Repeat(0, (int)N).Select(_ => Console.ReadLine().Split().Select(long.Parse).ToArray()).ToArray();
+            var ok = A.Max();
+            var ng = 0L;
+
+            while (1 < Math.Abs(ok - ng))
+            {
+                var m = (ok + ng) / 2;
+                var cnt = 0L;
+                for (int i = 0; i < N; i++)
+                {
+                    cnt += (long)Math.Ceiling((double)A[i] / (double)m) - 1;
+                }
+
+                if (cnt <= K)
+                    ok = m;
+                else
+                    ng = m;
+
+            }
+            Console.WriteLine(ok);
+        }
+    }
+}
