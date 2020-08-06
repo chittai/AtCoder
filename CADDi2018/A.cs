85b993c79fd8048a66e70235ea9e13ee770cf208
布田遼平 <chittai1221@gmail.com>
2020-08-06 23:08:39 +0900

    Add A.cs & AC

---
+++ CADDi2018/A.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace CADDi2018
+{
+    class A
+    {
+        static void Main(string[] args)
+        {
+            //input
+            var N = Console.ReadLine();
+            Console.WriteLine(N.Count(x => x == '2'));
+        }
+    }
+}
