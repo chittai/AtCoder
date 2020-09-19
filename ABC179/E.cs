9f11811ea5b4ce039eb2c4a0120cc037501c5815
布田遼平 <chittai1221@gmail.com>
2020-09-20 02:54:52 +0900

    Modify E.cs & AC

--- ABC179/E.cs
+++ ABC179/E.cs
@@ -12,48 +12,75 @@ namespace ABC179
             var NXM = Console.ReadLine().Split().Select(long.Parse).ToArray();
             var N = NXM[0]; var X = NXM[1]; var M = NXM[2];
 
+            // make array
+            var array = new long[M + 100];
             var a_ = X;
-            var sum = X;
-            var count = 1L;
-            var Count = new long[M];
-            Count[a_]++;
-
-            for (int i = 0; i < M; i++)
+            array[0] = a_;
+            for (int i = 1; i < M; i++)
             {
                 a_ = (a_ * a_) % M;
-                if (Count[a_] + 1 == 3) break;
-                Count[a_]++;
-                count++;
-                sum += a_;
+                array[i] = a_;
             }
 
-            //Console.WriteLine(count);
+            //Console.WriteLine(string.Join(" ", array));
 
+            var t = Program.cycle_finding(array);
+            var s = t.Item1; var e = t.Item2; var len = t.Item3;
+            //Console.WriteLine("{0}, {1}", s, e);
+
+            N -= s;
             var ans = 0L;
-            var a = N / count;
-            var b = N % count;
+            for (int i = 0; i < s; i++)
+            {
+                ans += array[i];
+            }
+
+            var a = N / len;
+            var b = N % len;
+
+            var sum = 0L;
+            for (long i = s; i <= e; i++)
+            {
+                sum += array[i];
+            }
+
             ans += a * sum;
 
-            //Console.WriteLine(ans);
-
-            if (0 < b)
+            for (int i = 0; i < b; i++)
             {
-                ans += X;
-                a_ = X;
-                for (int i = 0; i < b - 1; i++)
-                {
-                    a_ = (a_ * a_) % M;
-                    ans += a_;
-                    //Console.WriteLine("ans: " + ans);
-                }
+                ans += array[s + i];
             }
 
-            //Console.WriteLine(count);
-            //Console.WriteLine(string.Join(" ", Count));
             Console.WriteLine(ans);
+
+
         }
 
         // 循環開始・終了位置
+        static Tuple<long, long, long> cycle_finding(long[] a)
+        {
+            var x_length = new long[a.Length].Select(x => x = -1).ToArray();
+            var s = 0L;
+            var e = 0L;
+
+            var len = 0;
+            var index = 0;
+            for (int i = 0; x_length[a[i]] == -1; i++)
+            {
+                x_length[a[i]] = len;
+                len++;
+                index = i;
+            }
+            var cycle_length = len - x_length[a[index + 1]];
+
+            s = x_length[a[index + 1]];
+            e = x_length[a[index]];
+            //Console.WriteLine("{0}, {1}", s, e);
+            //Console.WriteLine(string.Join(" ", x_length));
+
+            var tuple = Tuple.Create(s, e, cycle_length);
+            return tuple;
+        }
 
     }
 }
